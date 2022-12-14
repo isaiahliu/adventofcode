b730b2240694a6d0dada9bf17aba77c0c1c57ab8
isaiahliu <ziqiang.liu@enoch-car.com>
2022-12-14 10:19:56 +0800

    Day01

---
+++ src/main/kotlin/y2017/Day01.kt
@@ -0,0 +1,13 @@
+package y2017
+
+import util.input
+
+fun main() {
+    val line = input.first()
+
+    val part1Result = line.filterIndexed { index, value -> value == line[(index + 1) % line.length] }.sumOf { it - '0' }
+    println(part1Result)
+
+    val part2Result = line.filterIndexed { index, value -> value == line[(index + line.length / 2) % line.length] }.sumOf { it - '0' }
+    println(part2Result)
+}
