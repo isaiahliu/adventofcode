aa1868d56fbd9ab217e504035f9e46b19ec8ab0b
isaiahliu <ziqiang.liu@enoch-car.com>
2022-12-14 15:01:56 +0800

    Day14

--- src/main/kotlin/y2022/Day14.kt
+++ src/main/kotlin/y2022/Day14.kt
@@ -5,69 +5,50 @@ import util.input
 fun main() {
     val walls = input.map { it.split(" -> ").map { it.split(",").let { it[0].toInt() to it[1].toInt() } } }
 
-    Space.initializeWalls(walls)
+    val space: MutableMap<Int, MutableMap<Int, Char>> = hashMapOf()
 
-    while (Space.dropSand(500, 0)) {
-//        Space.print()
-    }
-
-    println(Space.map.values.sumOf {
-        it.values.count { it == Space.SAND_ON_WALL }
-    })
-
-    println(Space.map.values.sumOf {
-        it.values.count { it == Space.SAND_ON_WALL || it == Space.SAND_ON_FLOOR }
-    })
-}
-
-
-private object Space {
-    private const val EMPTY = '.'
-    private const val WALL = '#'
-    private const val FLOOR = '='
-    const val SAND_ON_WALL = 'O'
-    const val SAND_ON_FLOOR = 'o'
+    val empty = '.'
+    val wall = '#'
+    val floor = '='
+    val sandOnWall = 'O'
+    val sandOnFloor = 'o'
 
     var maxY = 0
-    var currentSand = SAND_ON_WALL
-
-    fun initializeWalls(walls: List<List<Pair<Int, Int>>>) {
-        walls.forEach {
-            for (index in 0 until it.size - 1) {
-                val first = it[index]
-                val second = it[index + 1]
-
-                for (x in first.first.coerceAtMost(second.first)..first.first.coerceAtLeast(second.first)) {
-                    for (y in first.second.coerceAtMost(second.second)..first.second.coerceAtLeast(second.second)) {
-                        maxY = maxY.coerceAtLeast(y + 2)
-                        map[x, y] = WALL
-                    }
-                }
-            }
-        }
-    }
-
-    val map: MutableMap<Int, MutableMap<Int, Char>> = hashMapOf()
+    var currentSand = sandOnWall
 
     operator fun MutableMap<Int, MutableMap<Int, Char>>.get(x: Int, y: Int): Char {
-        return map.computeIfAbsent(x) { hashMapOf() }.computeIfAbsent(y) {
+        return computeIfAbsent(x) { hashMapOf() }.computeIfAbsent(y) {
             if (y < maxY) {
-                EMPTY
+                empty
             } else {
-                FLOOR
+                floor
             }
         }
     }
 
     operator fun MutableMap<Int, MutableMap<Int, Char>>.set(x: Int, y: Int, value: Char) {
-        map.computeIfAbsent(x) { hashMapOf() }[y] = value
+        computeIfAbsent(x) { hashMapOf() }[y] = value
+    }
+
+    walls.forEach {
+        for (index in 0 until it.size - 1) {
+            val first = it[index]
+            val second = it[index + 1]
+
+            for (x in first.first.coerceAtMost(second.first)..first.first.coerceAtLeast(second.first)) {
+                for (y in first.second.coerceAtMost(second.second)..first.second.coerceAtLeast(second.second)) {
+                    maxY = maxY.coerceAtLeast(y + 2)
+                    space[x, y] = wall
+                }
+            }
+        }
     }
 
     fun print() {
         for (y in 0..maxY) {
             buildString {
-                for (x in map.keys.min()..map.keys.max()) {
-                    append(map[x, y])
+                for (x in space.keys.min()..space.keys.max()) {
+                    append(space[x, y])
                 }
             }.also { println(it) }
         }
@@ -75,7 +56,7 @@ private object Space {
     }
 
     fun dropSand(startX: Int, startY: Int): Boolean {
-        if (map[startX, startY] != EMPTY) {
+        if (space[startX, startY] != empty) {
             return false
         }
 
@@ -83,31 +64,43 @@ private object Space {
         var y = startY
         while (true) {
             when {
-                map[x, y + 1] == EMPTY -> {
+                space[x, y + 1] == empty -> {
                     y++
                 }
 
-                map[x - 1, y + 1] == EMPTY -> {
+                space[x - 1, y + 1] == empty -> {
                     y++
                     x--
                 }
 
-                map[x + 1, y + 1] == EMPTY -> {
+                space[x + 1, y + 1] == empty -> {
                     y++
                     x++
                 }
 
-                (currentSand != SAND_ON_FLOOR) && (map[x - 1, y + 1] == FLOOR || map[x, y + 1] == FLOOR || map[x + 1, y + 1] == FLOOR) -> {
-                    currentSand = SAND_ON_FLOOR
+                (currentSand != sandOnFloor) && (space[x - 1, y + 1] == floor || space[x, y + 1] == floor || space[x + 1, y + 1] == floor) -> {
+                    currentSand = sandOnFloor
                 }
 
                 else -> {
-                    map[x, y] = currentSand
+                    space[x, y] = currentSand
                     break
                 }
             }
         }
         return true
     }
+
+    while (dropSand(500, 0)) {
+//        print()
+    }
+
+    println(space.values.sumOf {
+        it.values.count { it == sandOnWall }
+    })
+
+    println(space.values.sumOf {
+        it.values.count { it == sandOnWall || it == sandOnFloor }
+    })
 }
 
