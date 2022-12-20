67cca241321a8ce4bf871ba00d59e07c65bf0007
isaiahliu <ziqiang.liu@enoch-car.com>
2022-12-20 15:36:20 +0800

    Day24

---
+++ src/main/kotlin/y2017/Day24.kt
@@ -0,0 +1,20 @@
+package y2017
+
+import util.input
+
+fun main() {
+    val nodes = input.map { it.split("/").map { it.toInt() } }
+
+    val lengthResults = hashMapOf<Int, Int>()
+    fun walk(sum: Int, length: Int, currentNum: Int, nodes: List<List<Int>>): Int {
+        return nodes.filter { it.contains(currentNum) }.maxOfOrNull {
+            walk(sum + it.sum(), length + 1, it.fold(currentNum, Int::xor), nodes.toMutableList().also { list -> list -= it })
+        } ?: run {
+            lengthResults[length] = (lengthResults[length] ?: 0).coerceAtLeast(sum)
+            sum
+        }
+    }
+
+    println(walk(0, 0, 0, nodes))
+    println(lengthResults[lengthResults.keys.max()])
+}
