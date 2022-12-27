b92feb526ee88432c939d09f9b244a257addd750
isaiahliu <ziqiang.liu@enoch-car.com>
2022-12-27 12:36:16 +0800

    Day25

--- src/main/kotlin/y2022/Day25.kt
+++ src/main/kotlin/y2022/Day25.kt
@@ -5,21 +5,21 @@ import util.input
 fun main() {
     val maxDigits = input.maxOf { it.length }
 
-    val baseNum = String(CharArray(maxDigits) { '2' }).toLong(5)
+    val baseNum = CharArray(maxDigits) { '2' }.concatToString().toLong(5)
 
     val digits1 = "01234"
     val digits2 = "=-012"
 
     fun convertTo(num: Long): String {
-        return String((num + baseNum).toString(5).map {
+        return (num + baseNum).toString(5).map {
             digits2[digits1.indexOf(it)]
-        }.toCharArray()).trimStart('0')
+        }.toCharArray().concatToString().trimStart('0')
     }
 
     fun convertFrom(num: String): Long {
-        return String(num.padStart(maxDigits, '0').map {
+        return num.padStart(maxDigits, '0').map {
             digits1[digits2.indexOf(it)]
-        }.toCharArray()).toLong(5) - baseNum
+        }.toCharArray().concatToString().toLong(5) - baseNum
     }
 
     println(convertTo(input.sumOf { convertFrom(it) }))
