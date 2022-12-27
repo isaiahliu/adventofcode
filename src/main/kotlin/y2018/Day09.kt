90363cdb82008b6b060fd1ecbd5089fc1c0def41
isaiahliu <ziqiang.liu@enoch-car.com>
2022-12-27 17:06:48 +0800

    Day09

---
+++ src/main/kotlin/y2018/Day09.kt
@@ -0,0 +1,61 @@
+package y2018
+
+import util.input
+
+fun main() {
+    val regex = "(\\d+) players; last marble is worth (\\d+) points".toRegex()
+    val (players, points) = input.first().let { regex.matchEntire(it) }?.groupValues?.drop(1)?.map { it.toInt() }.orEmpty()
+
+    fun process(times: Int): Long {
+        val scores = LongArray(players)
+
+        var playerIndex = 0
+
+        var currentNode = Day09Node(0).also {
+            it.previous = it
+            it.next = it
+        }
+
+        repeat(points * times + 1) {
+            if (it == 0) {
+                return@repeat
+            }
+
+            if (it % 23 == 0) {
+                scores[playerIndex] += it.toLong()
+
+                repeat(7) {
+                    currentNode = currentNode.previous
+                }
+
+                scores[playerIndex] += currentNode.value.toLong()
+
+                currentNode.previous.next = currentNode.next
+                currentNode.next.previous = currentNode.previous
+                currentNode = currentNode.next
+            } else {
+                val n1 = currentNode.next
+                val n2 = n1.next
+
+                val newNode = Day09Node(it)
+                n1.next = newNode
+                newNode.previous = n1
+                n2.previous = newNode
+                newNode.next = n2
+
+                currentNode = newNode
+            }
+
+            playerIndex = (playerIndex + 1) % players
+        }
+        return scores.max()
+    }
+
+    println(process(1))
+    println(process(100))
+}
+
+private class Day09Node(val value: Int) {
+    lateinit var previous: Day09Node
+    lateinit var next: Day09Node
+}
