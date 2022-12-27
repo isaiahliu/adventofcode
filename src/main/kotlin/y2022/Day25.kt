53764d16283be37d5fae373bdbb5299c94615a61
isaiahliu <ziqiang.liu@enoch-car.com>
2022-12-27 11:55:22 +0800

    Day24

--- src/main/kotlin/y2022/Day25.kt
+++ src/main/kotlin/y2022/Day25.kt
@@ -7,29 +7,18 @@ fun main() {
 
     val baseNum = String(CharArray(maxDigits) { '2' }).toLong(5)
 
+    val digits1 = "01234"
+    val digits2 = "=-012"
+
     fun convertTo(num: Long): String {
-        return String((num + baseNum).toString(5).map { c ->
-            when (c) {
-                '0' -> '='
-                '1' -> '-'
-                '2' -> '0'
-                '3' -> '1'
-                '4' -> '2'
-                else -> c
-            }
+        return String((num + baseNum).toString(5).map {
+            digits2[digits1.indexOf(it)]
         }.toCharArray()).trimStart('0')
     }
 
     fun convertFrom(num: String): Long {
-        return String(num.padStart(maxDigits, '0').map { c ->
-            when (c) {
-                '=' -> '0'
-                '-' -> '1'
-                '0' -> '2'
-                '1' -> '3'
-                '2' -> '4'
-                else -> c
-            }
+        return String(num.padStart(maxDigits, '0').map {
+            digits1[digits2.indexOf(it)]
         }.toCharArray()).toLong(5) - baseNum
     }
 
