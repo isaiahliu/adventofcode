aff11b97d23229d3c0d015fe159561760115533a
isaiahliu <isaiahliu@outlook.com>
2023-01-04 09:51:07 +0800

    Day08

--- src/main/kotlin/y2019/Day08.kt
+++ src/main/kotlin/y2019/Day08.kt
@@ -5,5 +5,44 @@ import util.input
 fun main() {
     val layers = arrayListOf<Array<IntArray>>()
 
-    input
+    var layerIndex = 0
+
+    val width = 25
+    val height = 6
+
+    val line = input.first()
+    while (true) {
+        val nodes = line.drop(layerIndex * width * height).take(width * height)
+
+        if (nodes.isEmpty()) {
+            break
+        }
+
+        layers += Array(height) { rowIndex ->
+            IntArray(width) { columnIndex ->
+                nodes[rowIndex * width + columnIndex] - '0'
+            }
+        }
+
+        layerIndex++
+    }
+
+    val layer = layers.minBy { it.sumOf { it.count { it == 0 } } }
+
+    println(layer.sumOf { it.count { it == 1 } } * layer.sumOf { it.count { it == 2 } })
+
+    val result = Array(height) { rowIndex ->
+        IntArray(width) { columnIndex ->
+            layers.map { it[rowIndex][columnIndex] }.first { it != 2 }
+        }
+    }
+
+    result.forEach {
+        println(it.joinToString("") {
+            when (it) {
+                0 -> "."
+                else -> "#"
+            }
+        })
+    }
 }
