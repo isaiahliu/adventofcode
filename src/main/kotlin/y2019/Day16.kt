acb9265d37c1d071b5681f1daea92d91332f3876
isaiahliu <ziqiang.liu@enoch-car.com>
2023-01-06 15:38:11 +0800

    Day16

--- src/main/kotlin/y2019/Day16.kt
+++ src/main/kotlin/y2019/Day16.kt
@@ -7,76 +7,79 @@ fun main() {
     val nums = input.map { it.map { it - '0' }.toIntArray() }.first()
 
     fun process(init: IntArray, phase: Int, times: Int, offset: Int, outputLength: Int): String {
-        val length = init.size * times
-        val cache = hashMapOf<Int, MutableMap<Int, Byte>>()
-        repeat(phase) {
-            cache[it + 1] = hashMapOf()
-        }
-
-        fun getNum(level: Int, index: Int): Byte {
-            if (index >= length) {
-                throw RuntimeException("Error")
+        return if (offset > init.size * times / 2) {
+            val length = nums.size * times - offset
+            val array = IntArray(length) {
+                nums[nums.size - (it % nums.size) - 1]
             }
 
-            val levelCache = cache[level]!!
+            repeat(phase) {
+                for (index in 1 until array.size) {
+                    array[index] = (array[index] + array[index - 1]) % 10
+                }
+            }
 
-            return levelCache[index] ?: run {
-                var sum = 0
+            array.takeLast(8).reversed().joinToString("") { it.toString() }
+        } else {
+            val length = init.size * times
 
-                val count = index + 1
+            val cache = hashMapOf<Int, MutableMap<Int, Byte>>()
+            repeat(phase) {
+                cache[it + 1] = hashMapOf()
+            }
 
-                var drop = count - 1
-
-                var scanIndex = 0
-
-                var mul = 1
-
-                while (scanIndex < length) {
-                    scanIndex += drop
-                    drop = count
-
-                    repeat(count.coerceAtMost(length - scanIndex)) {
-                        sum += mul * if (level == 1) {
-                            init[(scanIndex + it) % init.size]
-                        } else {
-                            getNum(level - 1, scanIndex + it).toInt()
-                        }
-                    }
-
-                    mul *= -1
-
-                    scanIndex += count
+            fun getNum(level: Int, index: Int): Byte {
+                if (index >= length) {
+                    throw RuntimeException("Error")
                 }
 
-                val value = (sum.absoluteValue % 10).toByte()
-                levelCache[index] = value
+                val levelCache = cache[level]!!
 
-                value
+                return levelCache[index] ?: run {
+                    var sum = 0
+
+                    val count = index + 1
+
+                    var drop = count - 1
+
+                    var scanIndex = 0
+
+                    var mul = 1
+
+                    while (scanIndex < length) {
+                        scanIndex += drop
+                        drop = count
+
+                        repeat(count.coerceAtMost(length - scanIndex)) {
+                            sum += mul * if (level == 1) {
+                                init[(scanIndex + it) % init.size]
+                            } else {
+                                getNum(level - 1, scanIndex + it).toInt()
+                            }
+                        }
+
+                        mul *= -1
+
+                        scanIndex += count
+                    }
+
+                    val value = (sum.absoluteValue % 10).toByte()
+                    levelCache[index] = value
+
+                    value
+                }
+            }
+
+            buildString {
+                repeat(outputLength) {
+                    val num = getNum(phase, it + offset)
+
+                    append(num.toString())
+                }
             }
         }
-
-        return buildString {
-            repeat(outputLength) {
-                val num = getNum(phase, it + offset)
-
-                append(num.toString())
-            }
-        }
     }
 
-    val result1 = process(nums, 100, 1, 0, 8)
-    println(result1)
-
-    val length = nums.size * 10000 - nums.take(7).reduce { acc, n -> acc * 10 + n }
-    val array = IntArray(length) {
-        nums[nums.size - (it % nums.size) - 1]
-    }
-
-    repeat(100) {
-        for (index in 1 until array.size) {
-            array[index] = (array[index] + array[index - 1]) % 10
-        }
-    }
-
-    println(array.takeLast(8).reversed().joinToString("") { it.toString() })
+    println(process(nums, 100, 1, 0, 8))
+    println(process(nums, 100, 10000, nums.take(7).reduce { acc, n -> acc * 10 + n }, 8))
 }
