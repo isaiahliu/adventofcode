73d14d5c95f1216e22ac07e2beb771f631e9f245
isaiahliu <ziqiang.liu@enoch-car.com>
2023-01-30 14:29:38 +0800

    Day06

---
+++ src/main/kotlin/y2021/Day06.kt
@@ -0,0 +1,27 @@
+package y2021
+
+import util.input
+
+fun main() {
+    val fishes = LongArray(9)
+    input.first().split(",").map { it.toInt() }.forEach {
+        fishes[it]++
+    }
+
+    var currentIndex = 0
+    var result1 = 0L
+
+    repeat(256) {
+        fishes[(currentIndex + 7) % 9] += fishes[currentIndex]
+
+        currentIndex++
+        currentIndex %= 9
+
+        if (it == 80 - 1) {
+            result1 = fishes.sum()
+        }
+    }
+
+    println(result1)
+    println(fishes.sum())
+}
