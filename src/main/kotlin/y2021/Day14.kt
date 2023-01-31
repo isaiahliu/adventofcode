6c582b74518c26f1b096801498efd8f8eeb6f71d
isaiahliu <ziqiang.liu@enoch-car.com>
2023-01-31 09:42:46 +0800

    Day14

--- src/main/kotlin/y2021/Day14.kt
+++ src/main/kotlin/y2021/Day14.kt
@@ -21,10 +21,6 @@ fun main() {
         }
     }
 
-    fun Pair<Char, Char>.cacheKey(depth: Int): String {
-        return "${first}${second}_${depth}"
-    }
-
     input.drop(2).map { it.split(" -> ").let { it[0][0] to it[0][1] to it[1][0] } }.forEach { (pair, c) ->
         map[pair] = c
     }
@@ -38,7 +34,7 @@ fun main() {
                 return null
             }
 
-            val cacheKey = pair.cacheKey(depth)
+            val cacheKey = "${pair.first}${pair.second}_${depth}"
 
             if (cacheKey in cache) {
                 return cache[cacheKey]
@@ -54,7 +50,7 @@ fun main() {
             walk(m to pair.second, depth - 1)?.also { result.merge(it) }
 
             cache[cacheKey] = result
-            
+
             return result
         }
 
