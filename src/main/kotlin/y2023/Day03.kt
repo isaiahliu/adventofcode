ea24e82d183b081a780e4ca68479c021dc298937
isaiahliu <isaiahliu@outlook.com>
2023-12-03 22:10:27 +0800

    Day03

--- src/main/kotlin/y2023/Day03.kt
+++ src/main/kotlin/y2023/Day03.kt
@@ -3,56 +3,41 @@ package y2023
 import util.input
 
 fun main() {
-    fun Pair<Pair<Int, Int>, Int>.adjacent(symbol: Pair<Int, Int>): Boolean {
-        val (p, num) = this
-        val (r, c) = p
+    val symbolNums = hashMapOf<Pair<Int, Int>, MutableList<Int>>()
 
-        val (symbolR, symbolC) = symbol
-        val length = num.toString().length
-
-        return r == symbolR && (c - 1 == symbolC || c + length == symbolC) || (c - 1..c + length).any {
-            r - 1 == symbolR && it == symbolC || r + 1 == symbolR && it == symbolC
-        }
-    }
-
-    val nums = arrayListOf<Pair<Pair<Int, Int>, Int>>()
-    val symbols = hashSetOf<Pair<Int, Int>>()
-    val times = hashSetOf<Pair<Int, Int>>()
     input.forEachIndexed { r, s ->
         var num = 0
-        s.forEachIndexed { c, ch ->
-            if (ch in '0'..'9') {
-                num *= 10
-                num += ch - '0'
-            } else {
-                if (num > 0) {
-                    nums.add(r to c - num.toString().length to num)
+        var length = 0
+        ("$s.").forEachIndexed { c, ch ->
+            when {
+                ch in '0'..'9' -> {
+                    num *= 10
+                    num += ch - '0'
+                    length++
                 }
 
-                num = 0
-
-                if (ch != '.') {
-                    symbols.add(r to c)
-
-                    if (ch == '*') {
-                        times.add(r to c)
+                length > 0 -> {
+                    ((c - length - 1..c).map {
+                        listOf(r - 1 to it, r + 1 to it)
+                    }.flatten() + setOf(r to c - length - 1, r to c)).firstOrNull { (r, c) ->
+                        input.getOrNull(r)?.getOrNull(c)?.takeIf { it !in '0'..'9' && it != '.' } != null
+                    }?.also {
+                        symbolNums.computeIfAbsent(it) { arrayListOf() }.add(num)
                     }
+
+                    num = 0
+                    length = 0
                 }
             }
         }
+    }
 
-        if (num > 0) {
-            nums.add(r to s.length - num.toString().length to num)
+    val part1Result = symbolNums.values.sumOf { it.sum() }
+
+    val part2Result =
+        symbolNums.filter { input[it.key.first][it.key.second] == '*' && it.value.size > 1 }.values.sumOf {
+            it.fold(1) { a, b -> a * b }.toInt()
         }
-    }
-
-    val part1Result = nums.filter { num ->
-        symbols.any { num.adjacent(it) }
-    }.sumOf { it.second }
-
-    val part2Result = times.sumOf { sym ->
-        nums.filter { it.adjacent(sym) }.takeIf { it.size > 1 }?.fold(1) { a, b -> a * b.second }?.toInt() ?: 0
-    }
 
     println(part1Result)
     println(part2Result)
