f52aed9d35b2fbeb677d4fa3203a2eb81add26db
isaiahliu <isaiahliu@outlook.com>
2023-12-13 14:20:34 +0800

    Day 13

---
+++ src/main/kotlin/y2023/Day13.kt
@@ -0,0 +1,69 @@
+package y2023
+
+import util.input
+
+fun main() {
+    var part1Result = 0L
+    var part2Result = 0L
+
+    val strs = arrayListOf<String>()
+
+    fun Array<CharArray>.findMirror(maxDiff: Int): Int? {
+        return (1 until size).firstOrNull {
+            var index1 = it - 1
+            var index2 = it
+
+            var diffCount = 0
+            while (index1 >= 0 && index2 < size) {
+                val row1 = this[index1--]
+                val row2 = this[index2++]
+
+                diffCount += row1.indices.count { row1[it] != row2[it] }
+                if (diffCount > maxDiff) {
+                    return@firstOrNull false
+                }
+            }
+
+            diffCount == maxDiff
+        }
+    }
+
+    (input + "").forEach {
+        if (it.isEmpty()) {
+            val rows = Array(strs.size) { r ->
+                CharArray(strs[r].length) { c ->
+                    strs[r][c]
+                }
+            }
+
+            val columns = Array(strs[0].length) { c ->
+                CharArray(strs.size) { r ->
+                    strs[r][c]
+                }
+            }
+
+            rows.findMirror(0)?.also {
+                part1Result += it * 100
+            }
+
+            columns.findMirror(0)?.also {
+                part1Result += it
+            }
+
+            rows.findMirror(1)?.also {
+                part2Result += it * 100
+            }
+
+            columns.findMirror(1)?.also {
+                part2Result += it
+            }
+
+            strs.clear()
+        } else {
+            strs += it
+        }
+    }
+
+    println(part1Result)
+    println(part2Result)
+}
