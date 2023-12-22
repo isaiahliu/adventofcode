01b471f7621da3fb816e3be57b3281d44297d7b8
isaiahliu <isaiahliu@outlook.com>
2023-12-22 16:50:44 +0800

    Day 22

---
+++ src/main/kotlin/y2023/Day22.kt
@@ -0,0 +1,80 @@
+package y2023
+
+import util.expectInt
+import util.input
+import kotlin.math.sign
+
+fun main() {
+    expectInt {
+        class Brick(line: String) {
+            var x: IntRange
+            var y: IntRange
+            var z: IntRange
+
+            init {
+                line.split("~").also {
+                    val (x1, y1, z1) = it[0].split(",").map { it.toInt() }
+                    val (x2, y2, z2) = it[1].split(",").map { it.toInt() }
+
+                    x = x1..x2
+                    y = y1..y2
+                    z = z1..z2
+                }
+            }
+
+            val children = hashSetOf<Brick>()
+            val parents = hashSetOf<Brick>()
+        }
+
+        val bricks = arrayListOf<Brick>()
+        input.forEach { bricks.add(Brick(it)) }
+        bricks.sortBy { it.z.first() }
+
+        val lowest = bricks[0].z.first - 1
+
+        val heights = hashMapOf<Pair<Int, Int>, Brick>()
+
+        bricks.forEach { brick ->
+            val parents = hashSetOf<Brick>()
+            var maxZ = lowest
+            brick.x.forEach { x ->
+                brick.y.forEach { y ->
+                    heights[x to y]?.also {
+                        if (it.z.last > maxZ) {
+                            maxZ = it.z.last
+                            parents.clear()
+                        }
+
+                        if (it.z.last == maxZ) {
+                            parents.add(it)
+                        }
+                    }
+
+                    heights[x to y] = brick
+                }
+            }
+
+            val zSize = brick.z.last - brick.z.first
+            brick.z = maxZ + 1..maxZ + 1 + zSize
+
+            parents.forEach { it.children += brick }
+            brick.parents += parents
+        }
+
+        bricks.forEachIndexed { index, brick ->
+            val removed = hashSetOf(brick)
+            var result = 0
+            for (j in index + 1 until bricks.size) {
+                val checkBrick = bricks[j]
+
+                if (checkBrick.parents.isNotEmpty() && checkBrick.parents.all { it in removed }) {
+                    result++
+                    removed += checkBrick
+                }
+            }
+
+            part1Result += result.sign xor 1
+            part2Result += result
+        }
+    }
+}
