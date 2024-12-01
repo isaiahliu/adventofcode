0844287906c80a97500e8214e34a0ab8538b4944
isaiahliu <isaiahliu@outlook.com>
2024-12-01 22:48:35 +0800

    1

--- src/main/kotlin/y2024/Day01.kt
+++ src/main/kotlin/y2024/Day01.kt
@@ -2,14 +2,28 @@ package y2024
 
 import util.expect
 import util.input
+import java.util.*
+import kotlin.math.absoluteValue
 
 fun main() {
     expect(0, 0) {
-        input.forEach {
-            it.toIntOrNull()?.also {
-                part1Result += it
-                part2Result -= it
-            }
+        val leftQueue = PriorityQueue<Int>()
+        val rightQueue = PriorityQueue<Int>()
+
+        val map = hashMapOf<Int, IntArray>()
+
+        input.map { it.split(" ").mapNotNull { it.toIntOrNull() } }.forEach { (left, right) ->
+            leftQueue.add(left)
+            rightQueue.add(right)
+
+            map.computeIfAbsent(left) { intArrayOf(0, 0) }[0]++
+            map.computeIfAbsent(right) { intArrayOf(0, 0) }[1]++
         }
+
+        while (leftQueue.isNotEmpty()) {
+            part1Result += (leftQueue.poll() - rightQueue.poll()).absoluteValue
+        }
+
+        part2Result = map.entries.sumOf { (key, value) -> key * value[0] * value[1] }
     }
 }
