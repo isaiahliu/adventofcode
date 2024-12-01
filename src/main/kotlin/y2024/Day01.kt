292856d5343dd6fb24866e61c5e8ba98728c4b0f
isaiahliu <isaiahliu@outlook.com>
2024-12-01 23:00:14 +0800

    1

--- src/main/kotlin/y2024/Day01.kt
+++ src/main/kotlin/y2024/Day01.kt
@@ -11,19 +11,22 @@ fun main() {
         val rightQueue = PriorityQueue<Int>()
 
         val map = hashMapOf<Int, IntArray>()
-
         input.map { it.split(" ").mapNotNull { it.toIntOrNull() } }.forEach { (left, right) ->
             leftQueue.add(left)
             rightQueue.add(right)
 
-            map.computeIfAbsent(left) { intArrayOf(0, 0) }[0]++
-            map.computeIfAbsent(right) { intArrayOf(0, 0) }[1]++
+            map.computeIfAbsent(left) { intArrayOf(0, 0) }.also {
+                it[0]++
+                part2Result += left * it[1]
+            }
+            map.computeIfAbsent(right) { intArrayOf(0, 0) }.also {
+                it[1]++
+                part2Result += right * it[0]
+            }
         }
 
         while (leftQueue.isNotEmpty()) {
             part1Result += (leftQueue.poll() - rightQueue.poll()).absoluteValue
         }
-
-        part2Result = map.entries.sumOf { (key, value) -> key * value[0] * value[1] }
     }
 }
