45f8afe37f491aa7f2e334ef76719b037593b9a4
isaiahliu <isaiahliu@outlook.com>
2024-12-03 15:04:47 +0800

    1

---
+++ src/main/kotlin/y2024/Day03.kt
@@ -0,0 +1,28 @@
+package y2024
+
+import util.expect
+import util.input
+
+fun main() {
+    expect(0, 0) {
+        var t = 1
+        input.forEach { line ->
+            val regex = """(mul)\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)""".toRegex()
+
+            regex.findAll(line).forEach {
+                when {
+                    it.groupValues[1] == "mul" -> {
+                        (it.groupValues[2].toInt().toInt() * it.groupValues[3].toInt().toInt()).also {
+                            part1Result += it
+                            part2Result += it * t
+                        }
+                    }
+
+                    it.groupValues[4] == "do" -> t = 1
+                    it.groupValues[5] == "don't" -> t = 0
+                }
+
+            }
+        }
+    }
+}
