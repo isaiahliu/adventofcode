59d4cf635badd691910daa8d9664b1d0e8424a36
isaiahliu <isaiahliu@outlook.com>
2024-12-04 14:46:21 +0800

    1

--- src/main/kotlin/y2024/Day04.kt
+++ src/main/kotlin/y2024/Day04.kt
@@ -2,7 +2,6 @@ package y2024
 
 import util.expect
 import util.input
-import kotlin.math.absoluteValue
 import kotlin.math.sign
 
 fun main() {
@@ -28,7 +27,7 @@ fun main() {
                                 'S' -> 0b01.toInt()
                                 else -> 0b00.toInt()
                             } shl ((deltaR * deltaC) + 1)
-                        } - 0b1111).sign.absoluteValue xor 1
+                        } xor 0b1111).sign xor 1
                     }
                 }
             }
