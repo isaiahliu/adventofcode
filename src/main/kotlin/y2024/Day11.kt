a02f6e25daf323ded6d25f585bfeadb7e3003cda
isaiahliu <isaiahliu@outlook.com>
2024-12-11 14:07:23 +0800

    1

--- src/main/kotlin/y2024/Day11.kt
+++ src/main/kotlin/y2024/Day11.kt
@@ -10,18 +10,16 @@ fun main() {
         fun dfs(num: Long, depth: Int): Long {
             val numCache = cache.computeIfAbsent(num) { arrayOfNulls<Long>(76).also { it[0] = 1L } }
 
-            return numCache[depth] ?: run {
-                when {
-                    num == 0L -> dfs(1L, depth - 1)
+            return numCache[depth] ?: when {
+                num == 0L -> dfs(1L, depth - 1)
 
-                    num.toString().length % 2 == 0 -> num.toString().let {
-                        dfs(it.take(it.length / 2).toLong(), depth - 1) + dfs(it.drop(it.length / 2).toLong(), depth - 1)
-                    }
-
-                    else -> dfs(num * 2024, depth - 1)
-                }.also {
-                    numCache[depth] = it
+                num.toString().length % 2 == 0 -> num.toString().let {
+                    dfs(it.take(it.length / 2).toLong(), depth - 1) + dfs(it.drop(it.length / 2).toLong(), depth - 1)
                 }
+
+                else -> dfs(num * 2024, depth - 1)
+            }.also {
+                numCache[depth] = it
             }
         }
 
