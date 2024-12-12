26a58179f2759c288f7c7e609bd3c15ca1aa22f8
isaiahliu <isaiahliu@outlook.com>
2024-12-12 14:20:05 +0800

    1

--- src/main/kotlin/y2024/Day12.kt
+++ src/main/kotlin/y2024/Day12.kt
@@ -8,49 +8,6 @@ fun main() {
         class Group {
             var size = 0
 
-            val leftEdges = hashSetOf<Pair<Int, Int>>()
-            val rightEdges = hashSetOf<Pair<Int, Int>>()
-            val topEdges = hashSetOf<Pair<Int, Int>>()
-            val bottomEdges = hashSetOf<Pair<Int, Int>>()
-
-            var edgeCount: Int = 0
-
-            fun calculateEdges() {
-                arrayOf(leftEdges, rightEdges).forEach { edges ->
-                    while (edges.isNotEmpty()) {
-                        val (r, c) = edges.first()
-
-                        edges -= r to c
-
-                        arrayOf(-1, 1).forEach {
-                            var nr = r + it
-                            while (edges.remove(nr to c)) {
-                                nr += it
-                            }
-                        }
-
-                        edgeCount++
-                    }
-                }
-
-                arrayOf(topEdges, bottomEdges).forEach { edges ->
-                    while (edges.isNotEmpty()) {
-                        val (r, c) = edges.first()
-
-                        edges -= r to c
-
-                        arrayOf(-1, 1).forEach {
-                            var nc = c + it
-                            while (edges.remove(r to nc)) {
-                                nc += it
-                            }
-                        }
-
-                        edgeCount++
-                    }
-                }
-            }
-
             var innerParent: Group? = null
                 private set
 
@@ -58,10 +15,6 @@ fun main() {
                 set(value) {
                     innerParent = value
                     value.size += size
-                    value.leftEdges += leftEdges
-                    value.rightEdges += rightEdges
-                    value.topEdges += topEdges
-                    value.bottomEdges += bottomEdges
                 }
                 get() {
                     return innerParent?.parent?.also {
@@ -83,62 +36,64 @@ fun main() {
             Array(input[0].length) { c ->
                 Group().also {
                     it.size++
-
-                    when (r) {
-                        0 -> {
-                            it.topEdges += 0 to c
-                        }
-
-                        input.lastIndex -> {
-                            it.bottomEdges += input.size to c
-                        }
-                    }
-
-                    when (c) {
-                        0 -> {
-                            it.leftEdges += r to 0
-                        }
-
-                        input[0].lastIndex -> {
-                            it.rightEdges += r to input[0].length
-                        }
-                    }
                 }
             }
         }
 
         grids.forEachIndexed { r, row ->
             row.forEachIndexed { c, grid ->
-                if (input.getOrNull(r - 1)?.getOrNull(c) == input[r][c]) {
-                    grid.join(grids[r - 1][c])
-                } else {
-                    grid.parent.topEdges += r to c
-                    grids.getOrNull(r - 1)?.getOrNull(c)?.parent?.bottomEdges?.add(r to c)
-                }
-
-                if (input.getOrNull(r)?.getOrNull(c - 1) == input[r][c]) {
-                    grid.join(grids[r][c - 1])
-                } else {
-                    grid.parent.leftEdges += r to c
-                    grids[r].getOrNull(c - 1)?.parent?.rightEdges?.add(r to c)
+                arrayOf(r - 1 to c, r to c - 1).filter { (nr, nc) -> input.getOrNull(nr)?.getOrNull(nc) == input[r][c] }.forEach { (nr, nc) ->
+                    grid.join(grids[nr][nc])
                 }
             }
         }
 
         grids.forEachIndexed { r, row ->
             row.forEachIndexed { c, grid ->
-                arrayOf(r - 1 to c, r + 1 to c, r to c - 1, r to c + 1).forEach { (nr, nc) ->
-                    if (input.getOrNull(nr)?.getOrNull(nc) != input[r][c]) {
-                        part1Result += grid.parent.size
+                val area = grid.parent.size
+
+                val ch = input[r][c]
+                val topLeft = input.getOrNull(r - 1)?.getOrNull(c - 1)
+                val top = input.getOrNull(r - 1)?.getOrNull(c)
+                val topRight = input.getOrNull(r - 1)?.getOrNull(c + 1)
+                val left = input.getOrNull(r)?.getOrNull(c - 1)
+                val right = input.getOrNull(r)?.getOrNull(c + 1)
+                val bottomLeft = input.getOrNull(r + 1)?.getOrNull(c - 1)
+                val bottom = input.getOrNull(r + 1)?.getOrNull(c)
+
+                if (ch != top) {
+                    part1Result += area
+
+                    if (ch != left || left == topLeft) {
+                        part2Result += area
+                    }
+                }
+
+                if (ch != left) {
+                    part1Result += area
+
+                    if (ch != top || top == topLeft) {
+                        part2Result += area
+                    }
+                }
+
+                if (ch != bottom) {
+                    part1Result += area
+
+                    if (ch != left || left == bottomLeft) {
+                        part2Result += area
+                    }
+                }
+
+                if (ch != right) {
+                    part1Result += area
+
+                    if (ch != top || top == topRight) {
+                        part2Result += area
                     }
                 }
             }
         }
 
-        part2Result = grids.map { it.map { it.parent } }.flatten().distinct().sumOf {
-            it.calculateEdges()
-
-            it.edgeCount * it.size
-        }
     }
 }
