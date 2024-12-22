13e2041093d5da0f541e3688603a75691818aed1
isaiahliu <isaiahliu@outlook.com>
2024-12-22 17:22:21 +0800

    1

--- src/main/kotlin/y2024/Day21.kt
+++ src/main/kotlin/y2024/Day21.kt
@@ -14,16 +14,65 @@ fun main() {
             else -> 0
         }
 
-        //A, ^, >, v, <
-        val robotMovement = arrayOf(
-            arrayOf("", "<", "v", "<v", "v<<"),
-            arrayOf(">", "", "v>", "v", "v<"),
-            arrayOf("^", "<^", "", "<", "<<"),
-            arrayOf("^>", "^", ">", "", "<"),
-            arrayOf(">>^", ">^", ">>", ">", ""),
+        //Movement Priority: <^v> (Only if possible)
+        fun route(from: Pair<Int, Int>, to: Pair<Int, Int>, forbiddenNode: Pair<Int, Int>): String {
+            val (curR, curC) = from
+            val (targetR, targetC) = to
+
+            val deltaR = targetR - curR
+            val deltaC = targetC - curC
+
+            return buildString {
+                append("A")
+
+                val actions = arrayListOf<String>()
+                var reverse = false
+                if (deltaC < 0) {
+                    if (curR == forbiddenNode.first && curC + deltaC == forbiddenNode.second) {
+                        reverse = true
+                    }
+                    actions.add("<".repeat(-deltaC))
+                }
+
+                if (deltaR < 0) {
+                    if (actions.isEmpty() && curR + deltaR == forbiddenNode.first && curC == forbiddenNode.second) {
+                        reverse = true
+                    }
+
+                    actions.add("v".repeat(-deltaR))
+                }
+
+                if (deltaR > 0) {
+                    if (actions.isEmpty() && curR + deltaR == forbiddenNode.first && curC == forbiddenNode.second) {
+                        reverse = true
+                    }
+
+                    actions.add("^".repeat(deltaR))
+                }
+
+                if (deltaC > 0) {
+                    actions.add(">".repeat(deltaC))
+                }
+
+                if (reverse) {
+                    actions.reverse()
+                }
+
+                actions.forEach { append(it) }
+                append("A")
+            }
+        }
+
+        val robotPad = arrayOf(
+            1 to 2,
+            1 to 1,
+            0 to 2,
+            0 to 1,
+            0 to 0,
         )
 
         val dp = Array(26) { index ->
+            //A, ^, >, v, <
             Array(5) {
                 LongArray(5) { 1L - index.sign }
             }
@@ -35,7 +84,7 @@ fun main() {
 
             for (from in 0 until 5) {
                 for (to in 0 until 5) {
-                    "A${robotMovement[from][to]}A".reduce { last, cur ->
+                    route(robotPad[from], robotPad[to], 1 to 0).reduce { last, cur ->
                         curDp[from][to] += lastDp[last.operatorIndex()][cur.operatorIndex()]
                         cur
                     }
@@ -61,48 +110,7 @@ fun main() {
             var result = 0L
 
             str.fold('A') { cur, target ->
-                val (curR, curC) = doorPad[cur] ?: (0 to 0)
-                val (targetR, targetC) = doorPad[target] ?: (0 to 0)
-
-                val deltaR = targetR - curR
-                val deltaC = targetC - curC
-
-                buildString {
-                    append("A")
-
-                    val actions = arrayListOf<String>()
-                    var reverse = false
-                    if (deltaC < 0) {
-                        actions.add("<".repeat(-deltaC))
-
-                        if (curR == 0 && curC + deltaC == 0) {
-                            reverse = true
-                        }
-                    }
-
-                    if (deltaR < 0) {
-                        actions.add("v".repeat(-deltaR))
-
-                        if (curR + deltaR == 0 && curC == 0) {
-                            reverse = true
-                        }
-                    }
-
-                    if (deltaR > 0) {
-                        actions.add("^".repeat(deltaR))
-                    }
-
-                    if (deltaC > 0) {
-                        actions.add(">".repeat(deltaC))
-                    }
-
-                    if (reverse) {
-                        actions.reverse()
-                    }
-
-                    actions.forEach { append(it) }
-                    append("A")
-                }.reduce { last, cur ->
+                route(doorPad[cur] ?: (0 to 0), doorPad[target] ?: (0 to 0), 0 to 0).reduce { last, cur ->
                     result += dp[intermediateRobotCount][last.operatorIndex()][cur.operatorIndex()]
                     cur
                 }
@@ -114,8 +122,17 @@ fun main() {
         }
 
         input.forEach { str ->
-            part1Result += process(str, 2)
-            part2Result += process(str, 25)
+            val strNum = str.filter { it in '0'..'9' }.toCharArray().concatToString().toInt()
+
+            str.fold('A') { cur, target ->
+                route(doorPad[cur] ?: (0 to 0), doorPad[target] ?: (0 to 0), 0 to 0).reduce { last, cur ->
+                    part1Result += dp[2][last.operatorIndex()][cur.operatorIndex()] * strNum
+                    part2Result += dp[25][last.operatorIndex()][cur.operatorIndex()] * strNum
+                    cur
+                }
+
+                target
+            }
         }
     }
 }
