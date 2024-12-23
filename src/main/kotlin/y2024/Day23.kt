d536c1827a5a935565e10d7046782d636058e698
isaiahliu <isaiahliu@outlook.com>
2024-12-23 15:28:57 +0800

    1

--- src/main/kotlin/y2024/Day23.kt
+++ src/main/kotlin/y2024/Day23.kt
@@ -11,12 +11,9 @@ fun main() {
             adjacent.computeIfAbsent(from) { hashSetOf() } += to
             adjacent.computeIfAbsent(to) { hashSetOf() } += from
         }
+        var maxSize = 0
 
         class Trie(val node: String, val parent: Trie? = null, val size: Int) {
-            val children by lazy { hashMapOf<String, Trie>() }
-
-            var maxSize = Int.MAX_VALUE
-
             fun match(adj: Set<String>): Boolean {
                 return node.isEmpty() || node in adj && parent?.match(adj) == true
             }
@@ -25,55 +22,30 @@ fun main() {
                 return parent?.asString()?.let { "$it," }.orEmpty() + node
             }
 
-            fun markMaxSize(size: Int?) {
-                size?.also { maxSize = it } ?: run {
-                    maxSize = children.values.maxOf { it.maxSize }
-                }
-
-                parent?.markMaxSize(null)
-            }
-
             fun startsWithT(): Boolean {
                 return node.firstOrNull() == 't' || parent?.startsWithT() == true
             }
-        }
 
-        var maxSize = 0
-        fun dfs(trie: Trie) {
-            if (trie.size > maxSize) {
-                maxSize = trie.size
-                part2Result = trie.asString().drop(1)
-            }
-
-            if (trie.size == 3 && trie.startsWithT()) {
-                part1Result++
-            }
-
-            when {
-                trie.maxSize < maxSize -> {
-                    return
+            fun dfs() {
+                if (size > maxSize) {
+                    maxSize = size
+                    part2Result = asString().drop(1)
                 }
 
-                else -> {
-                    adjacent[trie.node]?.forEach { child ->
-                        if (child > trie.node && trie.match(adjacent[child].orEmpty())) {
-                            val childTrie = trie.children.computeIfAbsent(child) { Trie(child, trie, trie.size + 1) }
+                if (size == 3 && startsWithT()) {
+                    part1Result++
+                }
 
-                            dfs(childTrie)
-                        }
-                    }
-
-                    if (trie.children.isEmpty()) {
-                        trie.markMaxSize(trie.size)
+                adjacent[node]?.forEach { child ->
+                    if (child > node && match(adjacent[child].orEmpty())) {
+                        Trie(child, this, size + 1).dfs()
                     }
                 }
             }
         }
-
-        val root = Trie("", null, 0)
 
         adjacent[""] = adjacent.keys.toMutableSet()
 
-        dfs(root)
+        Trie("", null, 0).dfs()
     }
 }
