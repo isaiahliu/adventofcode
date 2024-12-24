5fd0851412c802480c790389cc1239f54728853b
isaiahliu <isaiahliu@outlook.com>
2024-12-24 16:38:59 +0800

    1

--- src/main/kotlin/y2024/Day24.kt
+++ src/main/kotlin/y2024/Day24.kt
@@ -5,10 +5,11 @@ import util.input
 
 fun main() {
     expect(0L, "") {
-        var x = 0L
-        var y = 0L
-
         val regex = "(.*) (.*) (.*) -> (.*)".toRegex()
+
+        fun Int.twoDigitStr(): String {
+            return this.toString().padStart(2, '0')
+        }
 
         abstract class AbstractGate(val name: String) {
             abstract val value: Int
@@ -16,52 +17,35 @@ fun main() {
             abstract fun asString(force: Boolean = false): String
         }
 
+        class LitGate(name: String, override val value: Int) : AbstractGate(name) {
+            override fun asString(force: Boolean): String {
+                return name
+            }
+        }
+
         val gates = hashMapOf<String, AbstractGate>()
         val replacement = hashMapOf<String, String>()
 
         class ExpGate(name: String, var op: String, var left: String, var right: String) : AbstractGate(name) {
-            private var _value: Int? = null
-
             override val value: Int
                 get() {
-                    if (_value == null) {
-                        _value = when (op) {
-                            "AND" -> (gates[left]?.value ?: 0) and (gates[right]?.value ?: 0)
-                            "OR" -> (gates[left]?.value ?: 0) or (gates[right]?.value ?: 0)
-                            "XOR" -> (gates[left]?.value ?: 0) xor (gates[right]?.value ?: 0)
-                            else -> throw Exception("Error")
-                        }
-
-                        if (name[0] == 'z') {
-                            part1Result += (_value?.toLong() ?: 0L) shl name.drop(1).toInt()
-                        }
+                    return when (op) {
+                        "AND" -> (gates[left]?.value ?: 0) and (gates[right]?.value ?: 0)
+                        "OR" -> (gates[left]?.value ?: 0) or (gates[right]?.value ?: 0)
+                        "XOR" -> (gates[left]?.value ?: 0) xor (gates[right]?.value ?: 0)
+                        else -> throw Exception("Error")
                     }
-                    return _value ?: 0
                 }
 
             private var _asString: String? = null
             override fun asString(force: Boolean): String {
                 if (force) {
                     _asString = arrayOf(gates[replacement[left] ?: left]?.asString(force), gates[replacement[right] ?: right]?.asString(force)).sortedBy { it }.let {
-                        "(${it[0]} ${op} ${it[1]})"
+                        "(${it[0]} $op ${it[1]})"
                     }
                 }
 
                 return _asString.orEmpty()
-            }
-        }
-
-        class LitGate(name: String, override val value: Int) : AbstractGate(name) {
-            init {
-                if (name[0] == 'x') {
-                    x += value.toLong() shl name.drop(1).toInt()
-                } else {
-                    y += value.toLong() shl name.drop(1).toInt()
-                }
-            }
-
-            override fun asString(force: Boolean): String {
-                return name
             }
         }
 
@@ -84,61 +68,62 @@ fun main() {
             }
         }
 
-        gates.values.forEach { it.value }
-
-        val indices = Array(64) {
-            it.toString().padStart(2, '0')
+        var zSize = 0
+        while (true) {
+            gates["z${zSize.twoDigitStr()}"]?.also {
+                part1Result += (1L shl zSize++) * it.value
+            } ?: break
         }
-        val dp = Array(indices.size) { arrayOf("(x${indices[it]} XOR y${indices[it]})", "(x${indices[it]} AND y${indices[it]})") }
+
+        val dp = Array(zSize) {
+            arrayOf(
+                "(x${it.twoDigitStr()} XOR y${it.twoDigitStr()})",
+                "(x${it.twoDigitStr()} AND y${it.twoDigitStr()})"
+            )
+        }
 
         for (i in 1 until dp.size) {
             dp[i][0] = "(${dp[i - 1][1]} XOR ${dp[i][0]})"
-            dp[i][1] = "((${dp[i - 1][1]} AND (x${indices[i]} XOR y${indices[i]})) OR ${dp[i][1]})"
+            dp[i][1] = "((${dp[i - 1][1]} AND (x${i.twoDigitStr()} XOR y${i.twoDigitStr()})) OR ${dp[i][1]})"
         }
 
-
-        next@ while (true) {
+        next@ while (replacement.size < 8) {
             val strMap = gates.values.associateBy { it.asString(true) }
 
-            var index = 0
+            var index = -1
 
-            while (replacement.size < 8) {
-                val gateName = "z${indices[index]}".let { replacement[it] ?: it }
-                val gate = gates[gateName] as? ExpGate ?: break@next
-                if (gate.asString() != dp[index][0]) {
-                    val anotherGate = strMap[dp[index][0]]
-                    if (anotherGate != null) {
-                        part2Result += gateName
-                        part2Result += anotherGate.name
+            while (true) {
+                val zGateName = "z${(++index).twoDigitStr()}".let { replacement[it] ?: it }
+                val gate = gates[zGateName]?.let { it as? ExpGate }?.takeIf { it.asString() != dp[index][0] } ?: continue
 
-                        replacement[gate.name] = anotherGate.name
-                        replacement[anotherGate.name] = gate.name
+                val targetGate = strMap[dp[index][0]]
+
+                when {
+                    targetGate != null -> {
+                        replacement[gate.name] = targetGate.name
+                        replacement[targetGate.name] = gate.name
+
                         continue@next
-                    } else if (gate.op == "XOR") {
-                        val left = gate.left
-                        val right = gate.right
+                    }
 
-                        val validPart = hashSetOf(dp[index - 1][1], "(x${indices[index]} XOR y${indices[index]})")
+                    gate.op == "XOR" -> {
+                        val validParts = hashSetOf(dp[index - 1][1], "(x${index.twoDigitStr()} XOR y${index.twoDigitStr()})")
 
                         when {
-                            validPart.remove(gates[left]?.asString()) -> {
-                                val r = strMap[validPart.first()] ?: throw Exception("Error")
-                                part2Result += r.name
-                                part2Result += right
+                            validParts.remove(gates[gate.left]?.asString()) -> {
+                                val r = strMap[validParts.first()] ?: throw Exception("Error")
 
-                                replacement[r.name] = right
-                                replacement[right] = r.name
+                                replacement[r.name] = gate.right
+                                replacement[gate.right] = r.name
 
                                 continue@next
                             }
 
-                            validPart.remove(gates[right]?.asString()) -> {
-                                val r = strMap[validPart.first()] ?: throw Exception("Error")
-                                part2Result += r.name
-                                part2Result += left
+                            validParts.remove(gates[gate.right]?.asString()) -> {
+                                val r = strMap[validParts.first()] ?: throw Exception("Error")
 
-                                replacement[r.name] = left
-                                replacement[left] = r.name
+                                replacement[r.name] = gate.left
+                                replacement[gate.left] = r.name
 
                                 continue@next
                             }
@@ -147,15 +132,13 @@ fun main() {
                                 throw Exception("Error")
                             }
                         }
-                    } else {
+                    }
+
+                    else -> {
                         throw Exception("Error")
                     }
                 }
-
-                index++
             }
-
-            break
         }
 
         part2Result = replacement.keys.sorted().joinToString(",")
