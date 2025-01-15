c75fbb780693a0cc7b8d97043871c8521df0086d
isaiahliu <isaiahliu@outlook.com>
2025-01-15 16:30:23 +0800

    1

--- src/main/kotlin/y2015/Day02.kt
+++ src/main/kotlin/y2015/Day02.kt
@@ -1,21 +1,13 @@
 package y2015
 
+import util.expect
 import util.input
 
 fun main() {
-    var part1Sum = 0
-    var part2Sum = 0
-    input.map { it.split("x").map { it.toInt() }.sorted() }.filter { it.size == 3 }.forEach { (a, b, c) ->
-        part1Sum += 2 * a * b
-        part1Sum += 2 * a * c
-        part1Sum += 2 * b * c
-        part1Sum += a * b
-
-        part2Sum += a * 2
-        part2Sum += b * 2
-        part2Sum += a * b * c
+    expect(0, 0) {
+        input.map { it.split("x").map { it.toInt() }.sorted() }.filter { it.size == 3 }.forEach { (a, b, c) ->
+            part1Result += 3 * a * b + 2 * a * c + 2 * b * c
+            part2Result += a * 2 + b * 2 + a * b * c
+        }
     }
-
-    println(part1Sum)
-    println(part2Sum)
 }
