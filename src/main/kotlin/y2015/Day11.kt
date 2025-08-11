e549716d86690491bdf3300be82a4d28b863c903
isaiahliu <isaiahliu@outlook.com>
2025-08-11 17:37:02 +0800

    1

--- src/main/kotlin/y2015/Day11.kt
+++ src/main/kotlin/y2015/Day11.kt
@@ -1,64 +1,60 @@
 package y2015
 
+import util.expect
 import util.input
 
 fun main() {
-    val chars = input.first().toCharArray()
+    expect("", "") {
+        val chars = input.first().toCharArray()
 
-    while (true) {
         chars.increase()
+        part1Result = chars.concatToString()
 
-        if (chars.valid) {
-            break
-        }
-    }
-
-    val part1Result = String(chars)
-
-    while (true) {
         chars.increase()
-
-        if (chars.valid) {
-            break
-        }
+        part2Result = chars.concatToString()
     }
-
-    val part2Result = String(chars)
-
-    println(part1Result)
-    println(part2Result)
 }
 
 private fun CharArray.increase() {
-    this[this.lastIndex]++
-    for (index in this.lastIndex downTo 0) {
-        if (this[index] <= 'z') {
-            return
-        } else {
-            this[index] = 'a'
-            this[index - 1]++
+    do {
+        this[this.lastIndex]++
+
+        for (index in this.lastIndex downTo 0) {
+            if (this[index] <= 'z') {
+                break
+            } else {
+                this[index] = 'a'
+                this[index - 1]++
+            }
         }
-    }
+    } while (!valid)
 }
 
-private val invalidChars = arrayOf('i', 'o', 'l')
+private val invalidChars = setOf('i', 'o', 'l')
 
 private val CharArray.valid: Boolean
     get() {
         var seq = false
         val dup = hashSetOf<Char>()
-        for ((index, c) in this.withIndex()) {
-            if (c in invalidChars) {
-                return false
+        var diff2 = -1
+        var prev = Char(0)
+        for (c in this) {
+            when (c) {
+                in invalidChars -> {
+                    return false
+                }
+
+                prev -> {
+                    dup += c
+                }
+
+                prev + 1 -> {
+                    seq = diff2 == 1
+                }
             }
 
-            if (index > 0 && c == this[index - 1]) {
-                dup += c
-            }
-
-            if (!seq && index > 1 && c - this[index - 1] == 1 && c - this[index - 2] == 2) {
-                seq = true
-            }
+            diff2 = c - prev
+            prev = c
         }
 
         return seq && dup.size >= 2
