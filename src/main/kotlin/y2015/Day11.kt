b4e0beb2d7762e3db1588499a35587deecb00de4
isaiahliu <isaiahliu@outlook.com>
2025-08-11 17:42:00 +0800

    1

--- src/main/kotlin/y2015/Day11.kt
+++ src/main/kotlin/y2015/Day11.kt
@@ -5,7 +5,54 @@ import util.input
 
 fun main() {
     expect("", "") {
+        val invalidChars = setOf('i', 'o', 'l')
+
         val chars = input.first().toCharArray()
+
+        fun CharArray.valid(): Boolean {
+            var seq = false
+            val dup = hashSetOf<Char>()
+            var diff2 = -1
+            var prev = Char(0)
+            for (c in this) {
+                when (c) {
+                    prev -> {
+                        dup += c
+                    }
+
+                    prev + 1 -> {
+                        seq = diff2 == 1
+                    }
+                }
+
+                diff2 = c - prev
+                prev = c
+            }
+
+            return seq && dup.size >= 2
+        }
+
+        fun CharArray.increase() {
+            do {
+                this[this.lastIndex]++
+
+                for (index in this.lastIndex downTo 0) {
+                    when {
+                        this[index] <= 'z' -> {
+                            if (this[index] in invalidChars) {
+                                this[index]++
+                            }
+                            break
+                        }
+
+                        else -> {
+                            this[index] = 'a'
+                            this[index - 1]++
+                        }
+                    }
+                }
+            } while (!valid())
+        }
 
         chars.increase()
         part1Result = chars.concatToString()
@@ -14,49 +61,4 @@ fun main() {
         part2Result = chars.concatToString()
     }
 }
-
-private fun CharArray.increase() {
-    do {
-        this[this.lastIndex]++
-
-        for (index in this.lastIndex downTo 0) {
-            if (this[index] <= 'z') {
-                break
-            } else {
-                this[index] = 'a'
-                this[index - 1]++
-            }
-        }
-    } while (!valid)
-}
-
-private val invalidChars = setOf('i', 'o', 'l')
-
-private val CharArray.valid: Boolean
-    get() {
-        var seq = false
-        val dup = hashSetOf<Char>()
-        var diff2 = -1
-        var prev = Char(0)
-        for (c in this) {
-            when (c) {
-                in invalidChars -> {
-                    return false
-                }
-
-                prev -> {
-                    dup += c
-                }
-
-                prev + 1 -> {
-                    seq = diff2 == 1
-                }
-            }
-
-            diff2 = c - prev
-            prev = c
-        }
-
-        return seq && dup.size >= 2
-    }
 
