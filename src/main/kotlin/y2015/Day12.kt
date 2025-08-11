4c7007016522fe44b45423238c9325dfe31e0042
isaiahliu <isaiahliu@outlook.com>
2025-08-11 19:06:51 +0800

    1

--- src/main/kotlin/y2015/Day12.kt
+++ src/main/kotlin/y2015/Day12.kt
@@ -1,61 +1,78 @@
 package y2015
 
+import util.expect
 import util.input
+import java.util.*
 
 fun main() {
-    val line = "{\"root\":${input.first()}}"
+    expect(0, 0) {
+        var num = 0
+        var neg = 1
 
-    val part1Sum = line.sumResult
+        var prev2 = Char(0)
+        var prev1 = Char(0)
 
-    var root: JsonNode? = null
+        val lefts = LinkedList<Char>()
+        var depth = 0
+        val sums = arrayOfNulls<Int>(999)
+        sums[0] = 0
 
-    var current = root
+        input.first().forEach {
+            if (it in '0'..'9') {
+                num *= 10
+                num += (it - '0') * neg
+                return@forEach
+            }
 
-    line.forEach {
-        when (it) {
-            '{' -> {
-                current = JsonNode(current)
-                if (root == null) {
-                    root = current
+            if (num != 0) {
+                part1Result += num
+
+                sums[depth] = sums[depth]?.let { it + num }
+                num = 0
+                neg = 1
+            }
+
+            when (it) {
+                '-' -> {
+                    neg = -1
+                }
+
+                '{' -> {
+                    depth++
+                    lefts.push(it)
+                    sums[depth] = 0
+                }
+
+                '}' -> {
+                    lefts.poll()
+
+                    sums[depth]?.also { add ->
+                        sums[depth - 1] = sums[depth - 1]?.let { it + add }
+                    }
+                    depth--
+                }
+
+                '[' -> {
+                    lefts.push(it)
+                }
+
+                ']' -> {
+                    lefts.poll()
+                }
+
+                'd' -> {
+                    if (lefts.peek() == '{' && prev2 == 'r' && prev1 == 'e') {
+                        sums[depth] = null
+                    }
                 }
             }
 
-            '}' -> {
-                current = current!!.parent
-            }
+            prev2 = prev1
+            prev1 = it
+        }
 
-            else -> {
-                current!!.str.append(it)
-            }
+        sums[0]?.also {
+            part2Result = it
         }
     }
-
-    println(part1Sum)
-    println(root!!.sum)
-}
-
-private val regex = "(-?\\d+)".toRegex()
-
-private val String.sumResult: Int
-    get() {
-        return regex.findAll(this).sumOf { it.groupValues[1].toInt() }
-    }
-
-private class JsonNode(val parent: JsonNode? = null) {
-    init {
-        parent?.children?.add(this)
-    }
-
-    var str: StringBuilder = StringBuilder()
-
-    val children = arrayListOf<JsonNode>()
-
-    val sum: Int
-        get() {
-            return if (str.contains(":\"red\"")) {
-                0
-            } else {
-                str.toString().sumResult + children.sumOf { it.sum }
-            }
-        }
 }
