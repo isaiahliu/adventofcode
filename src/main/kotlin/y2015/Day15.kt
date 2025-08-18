e5aaf5e525e24f8f80b6342c6b48af654ce1a248
isaiahliu <isaiahliu@outlook.com>
2025-08-18 11:00:52 +0800

    1

--- src/main/kotlin/y2015/Day15.kt
+++ src/main/kotlin/y2015/Day15.kt
@@ -1,66 +1,66 @@
 package y2015
 
+import util.expect
 import util.input
 
 fun main() {
-    val rsexp =
-        "\\w+: capacity (-?\\d+), durability (-?\\d+), flavor (-?\\d+), texture (-?\\d+), calories (-?\\d+)".toRegex()
+    expect(0, 0) {
+        val rsexp =
+            "\\w+: capacity (-?\\d+), durability (-?\\d+), flavor (-?\\d+), texture (-?\\d+), calories (-?\\d+)".toRegex()
 
-    val ingredients = arrayListOf<Ingredient>()
+        data class Ingredient(
+            val capacity: Int,
+            val durability: Int,
+            val flavor: Int,
+            val texture: Int,
+            val calories: Int,
+            var teaspoon: Int = 0
+        )
 
-    input.forEach {
-        val match = rsexp.matchEntire(it) ?: return
+        val ingredients = arrayListOf<Ingredient>()
 
-        val capacity = match.groupValues[1].toInt()
-        val durability = match.groupValues[2].toInt()
-        val flavor = match.groupValues[3].toInt()
-        val texture = match.groupValues[4].toInt()
-        val calories = match.groupValues[5].toInt()
+        input.forEach {
+            val match = rsexp.matchEntire(it) ?: return@forEach
 
-        ingredients += Ingredient(capacity, durability, flavor, texture, calories)
-    }
+            val capacity = match.groupValues[1].toInt()
+            val durability = match.groupValues[2].toInt()
+            val flavor = match.groupValues[3].toInt()
+            val texture = match.groupValues[4].toInt()
+            val calories = match.groupValues[5].toInt()
 
-    var part1Max = 0
-    var part2Max = 0
+            ingredients += Ingredient(capacity, durability, flavor, texture, calories)
+        }
 
-    fun walk(index: Int) {
-        val current = ingredients[index]
-        if (index == ingredients.lastIndex) {
-            current.teaspoon = 100 - ingredients.take(index).sumOf { it.teaspoon }
-
-            val capacity = ingredients.sumOf { it.capacity * it.teaspoon }.coerceAtLeast(0)
-            val durability = ingredients.sumOf { it.durability * it.teaspoon }.coerceAtLeast(0)
-            val flavor = ingredients.sumOf { it.flavor * it.teaspoon }.coerceAtLeast(0)
-            val texture = ingredients.sumOf { it.texture * it.teaspoon }.coerceAtLeast(0)
-            val calories = ingredients.sumOf { it.calories * it.teaspoon }.coerceAtLeast(0)
-
-            part1Max = part1Max.coerceAtLeast(capacity * durability * flavor * texture)
-
-            if (calories == 500) {
-                part2Max = part2Max.coerceAtLeast(capacity * durability * flavor * texture)
-            }
-        } else {
+        fun dfs(index: Int) {
+            val current = ingredients[index]
             val currentRemaining = 100 - ingredients.take(index).sumOf { it.teaspoon }
 
-            for (teaspoon in 0..currentRemaining) {
-                current.teaspoon = teaspoon
+            if (index == ingredients.lastIndex) {
+                current.teaspoon = currentRemaining
 
-                walk(index + 1)
+                val capacity = maxOf(ingredients.sumOf { it.capacity * it.teaspoon }, 0)
+                val durability = maxOf(ingredients.sumOf { it.durability * it.teaspoon }, 0)
+                val flavor = maxOf(ingredients.sumOf { it.flavor * it.teaspoon }, 0)
+                val texture = maxOf(ingredients.sumOf { it.texture * it.teaspoon }, 0)
+                val calories = maxOf(ingredients.sumOf { it.calories * it.teaspoon }, 0)
+
+                val score = capacity * durability * flavor * texture
+
+                part1Result = maxOf(part1Result, score)
+
+                if (calories == 500) {
+                    part2Result = maxOf(part2Result, score)
+                }
+            } else {
+                for (teaspoon in 0..currentRemaining) {
+                    current.teaspoon = teaspoon
+
+                    dfs(index + 1)
+                }
             }
         }
+
+        dfs(0)
     }
-
-    walk(0)
-
-    println(part1Max)
-    println(part2Max)
 }
 
-private data class Ingredient(
-    val capacity: Int,
-    val durability: Int,
-    val flavor: Int,
-    val texture: Int,
-    val calories: Int,
-    var teaspoon: Int = 0
-)
