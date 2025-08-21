7c915e24be997052957c16d447b7766d1946de3d
isaiahliu <isaiahliu@outlook.com>
2025-08-22 02:12:34 +0800

    1

--- src/main/kotlin/y2015/Day19.kt
+++ src/main/kotlin/y2015/Day19.kt
@@ -1,84 +1,78 @@
 package y2015
 
+import util.expect
 import util.input
 
 fun main() {
-    val regex = "(\\w+) => (\\w+)".toRegex()
+    expect(0, 0) {
+        class Trie(val length: Int = 0) {
+            val children = hashMapOf<Char, Trie>()
+            var replacements = hashSetOf<String>()
 
-    val replacements1 = arrayListOf<Pair<String, String>>()
-
-    input.forEach {
-        val match = regex.matchEntire(it) ?: return@forEach
-
-        replacements1 += match.groupValues[1] to match.groupValues[2]
-    }
-
-    val line = input.last()
-
-    val resultSet1 = hashSetOf<String>()
-
-    replacements1.forEach { pair ->
-        val nodes = line.split(pair.first)
-
-        if (nodes.size == 1) {
-            return@forEach
+            fun appendString(str: String, index: Int = 0): Trie {
+                return str.getOrNull(index)?.let {
+                    children.computeIfAbsent(it) { Trie(length + 1) }.appendString(str, index + 1)
+                } ?: this
+            }
         }
 
-        repeat(nodes.lastIndex) { pos ->
-            resultSet1 += buildString {
-                nodes.forEachIndexed { index, node ->
-                    if (index > 0) {
-                        if (index - 1 == pos) {
-                            append(pair.second)
-                        } else {
-                            append(pair.first)
-                        }
-                    }
-                    append(node)
+        val replacementRoot1 = Trie()
+        val replacements2 = arrayListOf<Pair<Int, Trie>>()
+
+        input.forEach {
+            it.split(" => ").takeIf { it.size == 2 }?.also { (from, to) ->
+                replacementRoot1.appendString(from).replacements += to
+
+                replacements2 += (to.length - from.length) to Trie().also {
+                    it.appendString(to).replacements += from
                 }
             }
         }
-    }
-    val part1Result = resultSet1.size
 
-    val replacements2 = replacements1.sortedByDescending { it.second.length - it.first.length }
+        replacements2.sortByDescending { it.first }
 
-    fun process(line: String, step: Int): Int {
-        if (line == "e") {
-            return step
-        }
+        fun process(line: String, replacementRoot: Trie): Set<String> {
+            val result = hashSetOf<String>()
 
-        for ((first, second) in replacements2) {
-            val nodes = line.split(second)
+            var replacementNodes = setOf(replacementRoot)
+            line.forEachIndexed { index, ch ->
+                replacementNodes = buildSet {
+                    add(replacementRoot)
 
-            if (nodes.size == 1) {
-                continue
-            }
-            repeat(nodes.lastIndex) {
-                val result = buildString {
-                    nodes.forEachIndexed { index, node ->
-                        if (index > 0) {
-                            append(
-                                if (index - 1 == it) {
-                                    first
-                                } else {
-                                    second
-                                }
-                            )
+                    replacementNodes.forEach {
+                        it.children[ch]?.also { replacementNode ->
+                            replacementNode.replacements.forEach { r ->
+                                result += line.replaceRange(index - replacementNode.length + 1, index + 1, r)
+                            }
+
+                            add(replacementNode)
                         }
-                        append(node)
                     }
                 }
-
-                process(result, step + 1).takeIf { it > 0 }?.also { return it }
             }
+
+            return result
         }
 
-        return -1
+        part1Result = process(input.last(), replacementRoot1).size
+
+        fun dfs(line: String, step: Int): Boolean {
+            if (line == "e") {
+                part2Result = step
+                return true
+            }
+
+            replacements2.forEach { (_, r) ->
+                process(line, r).forEach {
+                    if (dfs(it, step + 1)) {
+                        return true
+                    }
+                }
+            }
+
+            return false
+        }
+
+        dfs(input.last(), 0)
     }
-
-    val step = process(line, 0)
-
-    println(part1Result)
-    println(step)
 }
