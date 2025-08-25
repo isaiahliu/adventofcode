b38aa98c22d9b3c7f53e73854729d760e14ea0ca
isaiahliu <isaiahliu@outlook.com>
2025-08-25 15:40:38 +0800

    1

--- src/main/kotlin/y2016/Day01.kt
+++ src/main/kotlin/y2016/Day01.kt
@@ -1,68 +1,63 @@
 package y2016
 
+import util.expect
 import util.input
 import kotlin.math.absoluteValue
 
 fun main() {
-    var x = 0
-    var y = 0
+    expect(0, 0) {
+        var x = 0
+        var y = 0
 
-    val directions = intArrayOf(0, 1)
+        val visited = hashSetOf(0 to 0)
+        val visitedTwice = LinkedHashSet<Pair<Int, Int>>()
+        val visitedManyTimes = hashSetOf<Pair<Int, Int>>()
 
-    val regex = "(\\w)(\\d+)".toRegex()
+        fun visit() {
+            val pos = x to y
 
-    val visited = hashMapOf("0_0" to 1)
-    val visitedTwice = arrayListOf<Pair<String, Int>>()
-
-    fun visit() {
-        val pos = "${x}_${y}"
-
-        visited[pos] = (visited[pos] ?: 0) + 1
-
-        when (visited[pos]) {
-            1 -> {}
-            2 -> {
-                visitedTwice += pos to x.absoluteValue + y.absoluteValue
-            }
-
-            else -> {
-                visitedTwice.removeIf { it.first == pos }
+            when {
+                visited.add(pos) -> {}
+                pos in visitedManyTimes -> {}
+                visitedTwice.remove(pos) -> visitedManyTimes += pos
+                else -> visitedTwice += pos
             }
         }
+
+        val directions = intArrayOf(0, 1)
+        val regex = "(\\w)(\\d+)".toRegex()
+
+        regex.findAll(input.first()).map {
+            it.groupValues[1] to it.groupValues[2].toInt()
+        }.forEach { (direction, steps) ->
+            directions.reverse()
+
+            when (direction) {
+                "L" -> {
+                    directions[0] = -directions[0]
+                }
+
+                "R" -> {
+                    directions[1] = -directions[1]
+                }
+            }
+
+            repeat((directions[0] * steps).absoluteValue) {
+                x += directions[0]
+
+                visit()
+            }
+
+            repeat((directions[1] * steps).absoluteValue) {
+                y += directions[1]
+
+                visit()
+            }
+        }
+
+        part1Result = x.absoluteValue + y.absoluteValue
+        part2Result = visitedTwice.first().let { (x, y) -> x.absoluteValue + y.absoluteValue }
     }
-
-    input.first().split(", ").forEach {
-        val match = regex.matchEntire(it) ?: return
-
-        val direction = match.groupValues[1]
-        val steps = match.groupValues[2].toInt()
-
-        directions.reverse()
-
-        when (direction) {
-            "L" -> {
-                directions[0] = 0 - directions[0]
-            }
-
-            "R" -> {
-                directions[1] = 0 - directions[1]
-            }
-        }
-
-        repeat((directions[0] * steps).absoluteValue) {
-            x += directions[0]
-
-            visit()
-        }
-        repeat((directions[1] * steps).absoluteValue) {
-            y += directions[1]
-
-            visit()
-        }
-    }
-
-    println(x.absoluteValue + y.absoluteValue)
-    println(visitedTwice.first().second)
 }
 
 
