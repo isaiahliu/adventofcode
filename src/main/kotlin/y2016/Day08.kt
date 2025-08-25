abcc7b31fca3d1633dddeca2ab43bb020864c797
isaiahliu <isaiahliu@outlook.com>
2025-08-25 17:39:41 +0800

    1

--- src/main/kotlin/y2016/Day08.kt
+++ src/main/kotlin/y2016/Day08.kt
@@ -1,65 +1,59 @@
 package y2016
 
+import util.expect
 import util.input
 
 fun main() {
-    val width = 50
-    val height = 6
+    expect(0, "") {
+        val width = 50
+        val height = 6
 
-    val screen = Array(height) { Array(width) { " " } }
+        val screen = Array(height) { CharArray(width) { ' ' } }
 
-    fun rotateRow(rowIndex: Int) {
-        val t = screen[rowIndex][width - 1]
 
-        repeat(width - 1) {
-            screen[rowIndex][width - it - 1] = screen[rowIndex][width - it - 2]
-        }
-
-        screen[rowIndex][0] = t
-    }
-
-    fun rotateColumn(columnIndex: Int) {
-        val t = screen[height - 1][columnIndex]
-
-        repeat(height - 1) {
-            screen[height - it - 1][columnIndex] = screen[height - it - 2][columnIndex]
-        }
-
-        screen[0][columnIndex] = t
-    }
-
-    input.map { it.split(" ") }.forEach { nodes ->
-        when (nodes[0]) {
-            "rect" -> {
-                val (x, y) = nodes[1].split("x")
-                repeat(x.toInt()) { column ->
-                    repeat(y.toInt()) { row ->
-                        screen[row][column] = "#"
+        input.map { it.split(" ") }.forEach { nodes ->
+            when (nodes[0]) {
+                "rect" -> {
+                    val (x, y) = nodes[1].split("x")
+                    repeat(x.toInt()) { column ->
+                        repeat(y.toInt()) { row ->
+                            screen[row][column] = '#'
+                        }
                     }
                 }
-            }
 
-            else -> {
-                val index = nodes[2].split("=")[1].toInt()
-                val times = nodes[4].toInt()
+                else -> {
+                    val index = nodes[2].split("=")[1].toInt()
+                    val times = nodes[4].toInt()
 
-                repeat(times) {
-                    when (nodes[1]) {
-                        "row" -> {
-                            rotateRow(index)
-                        }
+                    repeat(times) {
+                        when (nodes[1]) {
+                            "row" -> {
+                                val t = screen[index][width - 1]
 
-                        "column" -> {
-                            rotateColumn(index)
+                                repeat(width - 1) {
+                                    screen[index][width - it - 1] = screen[index][width - it - 2]
+                                }
+
+                                screen[index][0] = t
+                            }
+
+                            "column" -> {
+                                val t = screen[height - 1][index]
+
+                                repeat(height - 1) {
+                                    screen[height - it - 1][index] = screen[height - it - 2][index]
+                                }
+
+                                screen[0][index] = t
+                            }
                         }
                     }
                 }
             }
         }
-    }
-    println(screen.sumOf { it.count { it.isNotBlank() } })
 
-    screen.forEach {
-        println(it.joinToString(""))
+        part1Result = screen.sumOf { it.count { it == '#' } }
+        part2Result = screen.joinToString("\n") { it.concatToString() }
     }
 }
