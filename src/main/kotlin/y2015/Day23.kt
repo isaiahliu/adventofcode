e2bf8c2e4b7b0d88ecdc327d9b831126a168e83c
isaiahliu <isaiahliu@outlook.com>
2025-08-25 14:30:23 +0800

    1

--- src/main/kotlin/y2015/Day23.kt
+++ src/main/kotlin/y2015/Day23.kt
@@ -1,53 +1,53 @@
 package y2015
 
+import util.expect
 import util.input
 
 fun main() {
-    val instructions = input.toTypedArray()
+    expect(0, 0) {
+        val instructions = input.map { it.split(" ").toTypedArray() }.toTypedArray()
 
-    fun execute(registers: IntArray): Int {
-        var instIndex = 0
-        while (instIndex < instructions.size) {
-            val nodes = instructions[instIndex].split(" ")
+        fun execute(registers: IntArray) {
+            var instIndex = 0
+            while (instIndex < instructions.size) {
+                val nodes = instructions[instIndex]
 
-            var instMovement = 1
-            when (nodes[0]) {
-                "hlf" -> {
-                    registers[nodes[1][0] - 'a'] /= 2
-                }
+                var instMovement = 1
+                when (nodes[0]) {
+                    "hlf" -> {
+                        registers[nodes[1][0] - 'a'] /= 2
+                    }
 
-                "tpl" -> {
-                    registers[nodes[1][0] - 'a'] *= 3
-                }
+                    "tpl" -> {
+                        registers[nodes[1][0] - 'a'] *= 3
+                    }
 
-                "inc" -> {
-                    registers[nodes[1][0] - 'a']++
-                }
+                    "inc" -> {
+                        registers[nodes[1][0] - 'a']++
+                    }
 
-                "jmp" -> {
-                    instMovement = nodes[1].toInt()
-                }
+                    "jmp" -> {
+                        instMovement = nodes[1].toInt()
+                    }
 
-                "jie" -> {
-                    if (registers[nodes[1][0] - 'a'] % 2 == 0) {
-                        instMovement = nodes[2].toInt()
+                    "jie" -> {
+                        if (registers[nodes[1][0] - 'a'] % 2 == 0) {
+                            instMovement = nodes[2].toInt()
+                        }
+                    }
+
+                    "jio" -> {
+                        if (registers[nodes[1][0] - 'a'] == 1) {
+                            instMovement = nodes[2].toInt()
+                        }
                     }
                 }
 
-                "jio" -> {
-                    if (registers[nodes[1][0] - 'a'] == 1) {
-                        instMovement = nodes[2].toInt()
-                    }
-                }
+                instIndex += instMovement
             }
-
-            instIndex += instMovement
         }
 
-        return registers[1]
+        part1Result = intArrayOf(0, 0).also { execute(it) }[1]
+        part2Result = intArrayOf(1, 0).also { execute(it) }[1]
     }
-
-    println(execute(intArrayOf(0, 0)))
-    println(execute(intArrayOf(1, 0)))
-
 }
