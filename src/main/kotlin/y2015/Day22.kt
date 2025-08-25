1ec80e8afd6873ba74d6ce0bd101de6d5157b647
isaiahliu <isaiahliu@outlook.com>
2025-08-25 14:25:46 +0800

    1

--- src/main/kotlin/y2015/Day22.kt
+++ src/main/kotlin/y2015/Day22.kt
@@ -1,168 +1,162 @@
 package y2015
 
+import util.expect
 import util.input
 
 fun main() {
-    var bossHitPoints = 0
-    var bossDamage = 0
+    expect(0, 0) {
+        data class Player(var hitPoint: Int, var mana: Int, var shieldTurns: Int = 0, var rechargeTurns: Int = 0, var totalCost: Int = 0)
+        data class Boss(var hitPoint: Int, val damage: Int, var poisonTurns: Int = 0)
 
-    input.forEach {
-        val (key, value) = it.split(": ")
-
-        value.toIntOrNull()?.also {
-            when (key) {
-                "Hit Points" -> bossHitPoints = it
-                "Damage" -> bossDamage = it
+        abstract class AbstractSpell(val cost: Int) {
+            open fun canCast(player: Player, boss: Boss): Boolean {
+                return player.mana >= cost
             }
-        }
-    }
 
-    val initPlayer = Player(50, 500)
-    val initBoss = Boss(bossHitPoints, bossDamage)
-
-    val spells = listOf(MagicMissileSpell(), DrainSpell(), ShieldSpell(), PoisonSpell(), RechargeSpell())
-
-    var result = Int.MAX_VALUE
-    fun nextTurn(playerTurn: Boolean, player: Player, boss: Boss, hardMode: Boolean = false) {
-        if (player.totalCost >= result) {
-            return
-        }
-
-        if (hardMode && playerTurn) {
-            player.hitPoint--
-
-            if (player.hitPoint <= 0) {
-                return
+            open fun cast(player: Player, boss: Boss) {
+                player.mana -= cost
+                player.totalCost += cost
             }
         }
 
-        if (player.rechargeTurns > 0) {
-            player.mana += 101
-            player.rechargeTurns--
-        }
-        if (player.shieldTurns > 0) {
-            player.shieldTurns--
-        }
+        class MagicMissileSpell : AbstractSpell(53) {
+            override fun cast(player: Player, boss: Boss) {
+                super.cast(player, boss)
 
-        if (boss.poisonTurns > 0) {
-            boss.hitPoint -= 3
-            boss.poisonTurns--
-
-            if (boss.hitPoint <= 0) {
-                result = player.totalCost.coerceAtMost(result)
-                return
+                boss.hitPoint -= 4
             }
         }
 
-        if (playerTurn) {
-            for (spell in spells.filter { it.canCast(player, boss) }) {
-                val playerCopy = player.copy()
-                val bossCopy = boss.copy()
+        class DrainSpell : AbstractSpell(73) {
+            override fun cast(player: Player, boss: Boss) {
+                super.cast(player, boss)
 
-                spell.cast(playerCopy, bossCopy)
+                player.hitPoint += 2
+                boss.hitPoint -= 2
+            }
+        }
 
-                if (bossCopy.hitPoint > 0) {
-                    nextTurn(false, playerCopy, bossCopy, hardMode)
-                } else {
-                    result = playerCopy.totalCost.coerceAtMost(result)
+        class ShieldSpell : AbstractSpell(113) {
+            override fun cast(player: Player, boss: Boss) {
+                super.cast(player, boss)
+
+                player.shieldTurns += 6
+            }
+
+            override fun canCast(player: Player, boss: Boss): Boolean {
+                return super.canCast(player, boss) && player.shieldTurns == 0
+            }
+        }
+
+        class PoisonSpell : AbstractSpell(173) {
+            override fun cast(player: Player, boss: Boss) {
+                super.cast(player, boss)
+
+                boss.poisonTurns += 6
+            }
+
+            override fun canCast(player: Player, boss: Boss): Boolean {
+                return super.canCast(player, boss) && boss.poisonTurns == 0
+            }
+        }
+
+        class RechargeSpell : AbstractSpell(229) {
+            override fun cast(player: Player, boss: Boss) {
+                super.cast(player, boss)
+
+                player.rechargeTurns += 5
+            }
+
+            override fun canCast(player: Player, boss: Boss): Boolean {
+                return super.canCast(player, boss) && player.rechargeTurns == 0
+            }
+        }
+
+        var bossHitPoints = 0
+        var bossDamage = 0
+
+        input.forEach {
+            val (key, value) = it.split(": ")
+
+            value.toIntOrNull()?.also {
+                when (key) {
+                    "Hit Points" -> bossHitPoints = it
+                    "Damage" -> bossDamage = it
                 }
             }
-        } else {
-            var damage = boss.damage
-            if (player.shieldTurns > 0) {
-                damage -= 7
-                damage = damage.coerceAtLeast(1)
-            }
-
-            player.hitPoint -= damage
-
-            if (player.hitPoint > 0) {
-                nextTurn(true, player, boss, hardMode)
-            }
         }
-    }
 
-    nextTurn(true, initPlayer.copy(), initBoss.copy())
-    val part1Result = result
-    result = Int.MAX_VALUE
+        val spells = arrayOf(MagicMissileSpell(), DrainSpell(), ShieldSpell(), PoisonSpell(), RechargeSpell())
 
-    nextTurn(true, initPlayer.copy(), initBoss.copy(), true)
-    val part2Result = result
+        fun battle(hardMode: Boolean): Int {
+            var result = Int.MAX_VALUE
+            fun dfs(playerTurn: Boolean, player: Player, boss: Boss) {
+                if (player.totalCost >= result) {
+                    return
+                }
 
-    println(part1Result)
-    println(part2Result)
-}
+                if (hardMode && playerTurn) {
+                    player.hitPoint--
 
-private data class Player(
-    var hitPoint: Int,
-    var mana: Int,
-    var shieldTurns: Int = 0,
-    var rechargeTurns: Int = 0,
-    var totalCost: Int = 0
-)
+                    if (player.hitPoint <= 0) {
+                        return
+                    }
+                }
 
-private data class Boss(var hitPoint: Int, val damage: Int, var poisonTurns: Int = 0)
+                if (player.rechargeTurns > 0) {
+                    player.mana += 101
+                    player.rechargeTurns--
+                }
 
-private abstract class AbstractSpell(val cost: Int) {
-    open fun canCast(player: Player, boss: Boss): Boolean {
-        return player.mana >= cost
-    }
+                if (player.shieldTurns > 0) {
+                    player.shieldTurns--
+                }
 
-    open fun cast(player: Player, boss: Boss) {
-        player.mana -= cost
-        player.totalCost += cost
-    }
-}
+                if (boss.poisonTurns > 0) {
+                    boss.hitPoint -= 3
+                    boss.poisonTurns--
 
-private class MagicMissileSpell : AbstractSpell(53) {
-    override fun cast(player: Player, boss: Boss) {
-        super.cast(player, boss)
+                    if (boss.hitPoint <= 0) {
+                        result = minOf(result, player.totalCost)
+                        return
+                    }
+                }
 
-        boss.hitPoint -= 4
-    }
-}
+                if (playerTurn) {
+                    for (spell in spells) {
+                        if (spell.canCast(player, boss)) {
+                            val playerCopy = player.copy()
+                            val bossCopy = boss.copy()
 
-private class DrainSpell : AbstractSpell(73) {
-    override fun cast(player: Player, boss: Boss) {
-        super.cast(player, boss)
+                            spell.cast(playerCopy, bossCopy)
 
-        player.hitPoint += 2
-        boss.hitPoint -= 2
-    }
-}
+                            if (bossCopy.hitPoint > 0) {
+                                dfs(false, playerCopy, bossCopy)
+                            } else {
+                                result = minOf(result, playerCopy.totalCost)
+                            }
+                        }
+                    }
+                } else {
+                    var damage = boss.damage
+                    if (player.shieldTurns > 0) {
+                        damage = maxOf(damage - 7, 1)
+                    }
 
-private class ShieldSpell : AbstractSpell(113) {
-    override fun cast(player: Player, boss: Boss) {
-        super.cast(player, boss)
+                    player.hitPoint -= damage
 
-        player.shieldTurns += 6
-    }
+                    if (player.hitPoint > 0) {
+                        dfs(true, player, boss)
+                    }
+                }
+            }
 
-    override fun canCast(player: Player, boss: Boss): Boolean {
-        return super.canCast(player, boss) && player.shieldTurns == 0
-    }
-}
+            dfs(true, Player(50, 500), Boss(bossHitPoints, bossDamage))
 
-private class PoisonSpell : AbstractSpell(173) {
-    override fun cast(player: Player, boss: Boss) {
-        super.cast(player, boss)
+            return result
+        }
 
-        boss.poisonTurns += 6
-    }
-
-    override fun canCast(player: Player, boss: Boss): Boolean {
-        return super.canCast(player, boss) && boss.poisonTurns == 0
-    }
-}
-
-private class RechargeSpell : AbstractSpell(229) {
-    override fun cast(player: Player, boss: Boss) {
-        super.cast(player, boss)
-
-        player.rechargeTurns += 5
-    }
-
-    override fun canCast(player: Player, boss: Boss): Boolean {
-        return super.canCast(player, boss) && player.rechargeTurns == 0
+        part1Result = battle(false)
+        part2Result = battle(true)
     }
 }
