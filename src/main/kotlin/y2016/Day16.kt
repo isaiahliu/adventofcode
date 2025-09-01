bbf45336a12daae666399f7242cad274b2a54c2f
isaiahliu <isaiahliu@outlook.com>
2025-09-01 16:35:45 +0800

    1

--- src/main/kotlin/y2016/Day16.kt
+++ src/main/kotlin/y2016/Day16.kt
@@ -1,29 +1,63 @@
 package y2016
 
+import util.expect
 import util.input
 
 fun main() {
-    var line = input.first()
-//    var line = "10000"
+    expect(StringBuilder(), StringBuilder()) {
+        class CheckSum(val size: Int, private val output: StringBuilder) {
+            var count = 0
+            var checkSum = 0
 
-    fun process(fitLength: Int): String {
-        while (line.length < fitLength) {
-            line = "${line}0${line.reversed().map { '1' - it }.joinToString("")}"
-        }
+            val child by lazy { CheckSum(size / 2, output) }
+            fun add(value: Int) {
+                if (size % 2 == 1) {
+                    output.append(value)
+                } else {
+                    count++
+                    checkSum += value
 
-        var checkSum = line.substring(0, fitLength)
-
-        while (checkSum.length % 2 == 0) {
-            checkSum = buildString {
-                for (index in checkSum.indices step 2) {
-                    append(1 - (checkSum[index] - '0').xor(checkSum[index + 1] - '0'))
+                    if (count == 2) {
+                        child.add((checkSum and 1) xor 1)
+                        checkSum = 0
+                        count = 0
+                    }
                 }
             }
         }
 
-        return checkSum
+        val base = input.first()
+        var baseIndex = -1
+        var baseIndexOffset = 1
+        var split = 1
+
+        val checkSums = arrayListOf(CheckSum(272, part1Result), CheckSum(35651584, part2Result))
+
+        repeat(35651584) {
+            baseIndex += baseIndexOffset
+
+            val num = base.getOrNull(baseIndex)?.let {
+                it - '0' xor ((baseIndexOffset + 1) / 2) xor 1
+            } ?: run {
+                baseIndexOffset = -baseIndexOffset
+                var num = split++
+                var s = 0
+                while (num > 1) {
+                    num.takeHighestOneBit().takeIf { it < num }?.also { higher ->
+                        num = higher * 2 - num
+
+                        s = s xor 1
+                    } ?: break
+                }
+                s
+            }
+
+            checkSums.forEach { checkSum ->
+                if (it < checkSum.size) {
+                    checkSum.add(num)
+                }
+            }
+        }
     }
-    println(process(272))
-    println(process(35651584))
 }
 
