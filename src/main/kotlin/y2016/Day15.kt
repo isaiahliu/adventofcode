3d7e8d27b8168fac2b7f2c8c339cda4e29f12ad1
isaiahliu <isaiahliu@outlook.com>
2025-09-01 12:11:13 +0800

    1

--- src/main/kotlin/y2016/Day15.kt
+++ src/main/kotlin/y2016/Day15.kt
@@ -1,41 +1,22 @@
 package y2016
 
+import util.expect
 import util.input
+import java.math.BigInteger
 
 fun main() {
-    val regex = "Disc #\\d+ has (\\d+) positions; at time=(\\d+), it is at position (\\d+).".toRegex()
+    expect(BigInteger.ZERO, BigInteger.ZERO) {
+        val regex = "Disc #\\d+ has (\\d+) positions; at time=(\\d+), it is at position (\\d+).".toRegex()
 
-    val discs = input.map {
-        val match = regex.matchEntire(it) ?: return
+        part2Result = (input.mapIndexed { index, line ->
+            regex.matchEntire(line)?.groupValues?.drop(1)?.map { it.toBigInteger() }?.let { (position, time, current) ->
+                position to (time - current - (index + 1).toBigInteger())
+            } ?: return@expect
+        } + (11.toBigInteger() to (-input.size - 1).toBigInteger()))
+            .reduce { (a, ma), (b, mb) ->
+                part1Result = ma
 
-        val (positionStr, time, current) = match.groupValues.drop(1)
-
-        val position = positionStr.toInt()
-
-        position to (current.toInt() - time.toInt()).mod(position)
+                a * b to (a * (mb - ma) * a.modInverse(b) + ma).mod(a * b)
+            }.second
     }
-
-    fun process(discs: List<Pair<Int, Int>>): Int {
-        var startTime = 0
-
-        while (true) {
-            if (discs.filterIndexed { discIndex, pair ->
-                    val passedSeconds = startTime + discIndex + 1
-
-                    (pair.second + passedSeconds) % pair.first > 0
-                }.isEmpty()) {
-                return startTime
-            }
-
-            startTime++
-        }
-    }
-    println(process(discs))
-
-    println(process(buildList {
-        addAll(discs)
-        add(11 to 0)
-    }))
 }
-
-private class Disc(val position: Int)
