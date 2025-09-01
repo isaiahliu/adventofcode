a7d9575955115d6391ca4edd79694495f31cc476
isaiahliu <isaiahliu@outlook.com>
2025-09-01 10:08:53 +0800

    1

--- src/main/kotlin/y2016/Day13.kt
+++ src/main/kotlin/y2016/Day13.kt
@@ -1,62 +1,38 @@
 package y2016
 
+import util.expect
 import util.input
+import java.util.*
 
 fun main() {
-    val favNum = input.first().toInt()
+    expect(0, 0) {
+        val favNum = input.first().toInt()
+        val target = 31 to 39
 
-    val targetX = 31
-    val targetY = 39
+        val visited = hashSetOf(1 to 1)
+        val tasks = LinkedList<Pair<Int, Int>>()
+        tasks.add(1 to 1)
 
-    fun Pair<Int, Int>.isOpen(): Boolean {
-        val (x, y) = this
-
-        return if (x < 0 || y < 0) {
-            false
-        } else {
-            (x * x + 3 * x + 2 * x * y + y + y * y + favNum).countOneBits() % 2 == 0
-        }
-    }
-
-    val walked = hashSetOf<String>()
-
-    val current = arrayListOf(1 to 1)
-
-    fun Pair<Int, Int>.canWalk(): Boolean {
-        return walked.add("${first}_${second}")
-    }
-
-    var step = 0
-
-    var part1Result = -1
-    var part2Result = -1
-
-    while (part1Result < 0 || part2Result < 0) {
-        if (step == 50) {
-            part2Result = walked.size
-        }
-        val tasks = current.toList()
-
-        current.clear()
-
-        for (pair in tasks) {
-            val (x, y) = pair
-
-            if (part1Result < 0 && x == targetX && y == targetY) {
-                part1Result = step
+        var step = 1
+        while (target !in visited || part2Result == 0) {
+            if (target !in visited) {
+                part1Result++
             }
 
-            (x - 1 to y).takeIf { it.isOpen() && it.canWalk() }?.also { current += it }
-            (x + 1 to y).takeIf { it.isOpen() && it.canWalk() }?.also { current += it }
-            (x to y - 1).takeIf { it.isOpen() && it.canWalk() }?.also { current += it }
-            (x to y + 1).takeIf { it.isOpen() && it.canWalk() }?.also { current += it }
+            repeat(tasks.size) {
+                val (px, py) = tasks.poll()
+
+                arrayOf(px - 1 to py, px + 1 to py, px to py - 1, px to py + 1).filter { (x, y) ->
+                    x >= 0 && y >= 0 && (x * x + 3 * x + 2 * x * y + y + y * y + favNum).countOneBits() % 2 == 0 && visited.add(x to y)
+                }.forEach {
+                    tasks.add(it)
+                }
+            }
+
+            if (step++ == 50) {
+                part2Result = visited.size
+            }
         }
-
-        step++
     }
-
-
-    println(part1Result)
-    println(part2Result)
 }
 
