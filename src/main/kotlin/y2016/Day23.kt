932dd4370c5e0cd1bb339232e602dd31ec62a566
isaiahliu <isaiahliu@outlook.com>
2025-09-25 17:28:19 +0800

    1

--- src/main/kotlin/y2016/Day23.kt
+++ src/main/kotlin/y2016/Day23.kt
@@ -1,82 +1,76 @@
 package y2016
 
+import util.expect
 import util.input
 
 fun main() {
-    var instructionIndex = 0
+    expect(0, 0) {
+        val toggleMap = hashMapOf(
+            "inc" to "dec", "dec" to "inc", "tgl" to "inc", "jnz" to "cpy", "cpy" to "jnz"
+        )
 
-    val registers = longArrayOf(12, 0, 0, 0)
+        fun process(initValue: Int): Int {
+            var instructionIndex = 0
+            val instructions = input.map { it.split(" ").toTypedArray() }
+            val registers = intArrayOf(initValue, 0, 0, 0)
 
-    fun String.evaluate(): Long {
-        return this.toLongOrNull() ?: registers[this[0] - 'a']
-    }
-
-    val instructions = input.map { it.split(" ").toTypedArray() }
-    while (instructionIndex < input.size) {
-        val instruction = instructions[instructionIndex]
-
-        when (instruction[0]) {
-            "cpy" -> {
-                registers[instruction[2][0] - 'a'] = instruction[1].evaluate()
-                instructionIndex++
+            fun String.evaluate(): Int {
+                return this.toIntOrNull() ?: registers[this[0] - 'a']
             }
 
-            "inc" -> {
-                registers[instruction[1][0] - 'a']++
-                instructionIndex++
-            }
+            while (instructionIndex < instructions.size) {
+                val instruction = instructions[instructionIndex]
 
-            "dec" -> {
-                registers[instruction[1][0] - 'a']--
-                instructionIndex++
-            }
+//            val sb = StringBuilder("${instructionIndex} ${instruction.joinToString(" ")}".padEnd(20, ' '))
+                when (instruction[0]) {
+                    "cpy" -> {
+                        registers[instruction[2][0] - 'a'] = instruction[1].evaluate()
+                        instructionIndex++
+                    }
 
-            "jnz" -> {
-                if (instruction[1].evaluate() != 0L) {
-                    instructionIndex += instruction[2].evaluate().toInt()
-                } else {
-                    instructionIndex++
-                }
-            }
+                    "inc" if instruction[1][0] == 'a' -> {
+                        registers[0] += registers[2] * registers[3]
+                        registers[2] = 1
+                        registers[3] = 1
+                        instructionIndex++
+                    }
 
-            "tgl" -> {
-                instructions.getOrNull(instructionIndex + instruction[1].evaluate().toInt())
-                    ?.also { toggleInstruction ->
-                        when (toggleInstruction.size) {
-                            2 -> {
-                                toggleInstruction[0] = when (toggleInstruction[0]) {
-                                    "inc" -> "dec"
-                                    else -> "inc"
-                                }
-                            }
+                    "inc" -> {
+                        registers[instruction[1][0] - 'a']++
+                        instructionIndex++
+                    }
 
-                            3 -> {
-                                toggleInstruction[0] = when (toggleInstruction[0]) {
-                                    "jnz" -> {
-                                        if (toggleInstruction[2].toIntOrNull() == null) {
-                                            "cpy"
-                                        } else {
-                                            toggleInstruction[0]
-                                        }
-                                    }
+                    "dec" -> {
+                        registers[instruction[1][0] - 'a']--
+                        instructionIndex++
+                    }
 
-                                    else -> {
-                                        "jnz"
-                                    }
-                                }
-                            }
+                    "jnz" -> {
+                        if (instruction[1].evaluate() != 0) {
+                            instructionIndex += instruction[2].evaluate().toInt()
+                        } else {
+                            instructionIndex++
                         }
                     }
 
-                instructionIndex++
+                    "tgl" -> {
+                        val targetInstructionIndex = instructionIndex + instruction[1].evaluate()
+                        instructions.getOrNull(targetInstructionIndex.toInt())?.also {
+                            it[0] = toggleMap[it[0]] ?: throw Exception("Error")
+                        }
+
+                        instructionIndex++
+                    }
+                }
             }
 
-            else -> {
-                println("nothing")
-                instructionIndex++
-            }
+//            registers.forEachIndexed { index, value ->
+//                sb.append("${'a' + index}:${value.toString().padEnd(8, ' ')}")
+//            }
+//            println(sb)
+            return registers[0]
         }
+        part1Result = process(7)
+        part2Result = process(12)
     }
-
-    println(registers[0])
 }
