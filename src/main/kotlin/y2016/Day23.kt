c5ea00caeeea2466374971ca4a1c7aa669b57234
isaiahliu <isaiahliu@outlook.com>
2025-09-25 17:33:41 +0800

    1

--- src/main/kotlin/y2016/Day23.kt
+++ src/main/kotlin/y2016/Day23.kt
@@ -9,6 +9,7 @@ fun main() {
             "inc" to "dec", "dec" to "inc", "tgl" to "inc", "jnz" to "cpy", "cpy" to "jnz"
         )
 
+        val showLog = false
         fun process(initValue: Int): Int {
             var instructionIndex = 0
             val instructions = input.map { it.split(" ").toTypedArray() }
@@ -21,53 +22,64 @@ fun main() {
             while (instructionIndex < instructions.size) {
                 val instruction = instructions[instructionIndex]
 
-//            val sb = StringBuilder("${instructionIndex} ${instruction.joinToString(" ")}".padEnd(20, ' '))
+                var instructionOffset = 1
+
                 when (instruction[0]) {
                     "cpy" -> {
                         registers[instruction[2][0] - 'a'] = instruction[1].evaluate()
-                        instructionIndex++
                     }
 
                     "inc" if instruction[1][0] == 'a' -> {
                         registers[0] += registers[2] * registers[3]
                         registers[2] = 1
                         registers[3] = 1
-                        instructionIndex++
                     }
 
                     "inc" -> {
                         registers[instruction[1][0] - 'a']++
-                        instructionIndex++
                     }
 
                     "dec" -> {
                         registers[instruction[1][0] - 'a']--
-                        instructionIndex++
                     }
 
                     "jnz" -> {
                         if (instruction[1].evaluate() != 0) {
-                            instructionIndex += instruction[2].evaluate().toInt()
-                        } else {
-                            instructionIndex++
+                            instructionOffset = instruction[2].evaluate()
                         }
                     }
 
                     "tgl" -> {
                         val targetInstructionIndex = instructionIndex + instruction[1].evaluate()
-                        instructions.getOrNull(targetInstructionIndex.toInt())?.also {
+                        instructions.getOrNull(targetInstructionIndex)?.also {
                             it[0] = toggleMap[it[0]] ?: throw Exception("Error")
                         }
-
-                        instructionIndex++
                     }
                 }
+
+                if (showLog) {
+                    buildString {
+                        append(instructionIndex)
+                        append(" ")
+                        instruction.forEach {
+                            append(it)
+                            append(" ")
+                        }
+                        repeat(20 - length) {
+                            append(" ")
+                        }
+
+                        registers.forEachIndexed { index, value ->
+                            append('a' + index)
+                            append(":")
+                            append(value.toString().padEnd(10, ' '))
+                        }
+                    }.also { println(it) }
+                }
+
+                instructionIndex += instructionOffset
             }
 
-//            registers.forEachIndexed { index, value ->
-//                sb.append("${'a' + index}:${value.toString().padEnd(8, ' ')}")
-//            }
-//            println(sb)
             return registers[0]
         }
         part1Result = process(7)
