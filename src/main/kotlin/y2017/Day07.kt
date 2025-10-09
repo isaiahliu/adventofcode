e51ee562f1c0c8fc3643521bf819453bfebe6662
isaiahliu <isaiahliu@outlook.com>
2025-10-09 21:05:57 +0800

    1

--- src/main/kotlin/y2017/Day07.kt
+++ src/main/kotlin/y2017/Day07.kt
@@ -6,91 +6,58 @@ import util.input
 fun main() {
     expect("", 0) {
         val children = hashMapOf<String, Set<String>>()
-        val parents = hashMapOf<String, String>()
         val weights = hashMapOf<String, Int>()
+        val leaves = hashSetOf<String>()
         input.map { it.split(" ") }.forEach {
             val name = it[0]
 
             weights[name] = it[1].trim('(', ')').toInt()
 
-            children[name] = it.drop(3).onEach {
+            children[name] = it.drop(3).map {
                 val childName = it.trim(',')
-                parents[childName] = name
+                leaves += childName
+                childName
             }.toMutableSet()
         }
 
-        part1Result = (children.keys - parents.keys).first()
-    }
-    val nodes = hashMapOf<String, Node>()
+        part1Result = (children.keys - leaves).first()
 
-    input.map { it.split(" ") }.forEach {
-        val name = it[0]
-        val node = nodes.computeIfAbsent(name) { Node(name) }
-
-        node.weight = it[1].trim('(', ')').toInt()
-
-        it.drop(3).forEach {
-            val childName = it.trim(',')
-            val child = nodes.computeIfAbsent(childName) { Node(childName) }
-
-            node.children += child
-            child.parent = node
+        val sums = hashMapOf<String, Int>()
+        fun sumDfs(node: String) {
+            sums[node] = (weights[node] ?: 0) + (children[node]?.sumOf {
+                sumDfs(it)
+                sums[it] ?: 0
+            } ?: 0)
         }
-    }
+        sumDfs(part1Result)
 
-    val root = nodes.values.first { it.parent == null }
+        fun dfs(node: String, diff: Int?): Int? {
+            val valueGroup = children[node]?.groupBy { sums[it] ?: 0 }.orEmpty()
 
-    println(root.name)
+            when {
+                valueGroup.size == 2 -> {
+                    val target = valueGroup.entries.first { it.value.size == 1 }
+                    val others = valueGroup.entries.first { it.value.size > 1 }
 
-    root.clearTotalWeight()
+                    return dfs(target.value.first(), others.key - target.key)
+                }
 
-    val unbalancedNodes = nodes.values.filter { it.children.map { it.totalWeight }.distinct().size > 1 }.toMutableList()
+                diff != null -> {
+                    return (weights[node] ?: 0) + diff
+                }
 
-    val additionalWeight = unbalancedNodes.firstOrNull { it.children.size > 2 }?.children?.let {
-        val weights = it.groupingBy { it.totalWeight }.eachCount().entries
-
-        val diffWeight = weights.first { it.value == 1 }.key
-        val balanceWeight = weights.first { it.value != 1 }.key
-
-        diffWeight - balanceWeight
-    } ?: 0
-
-    unbalancedNodes.toList().forEach {
-        it.parent?.also { unbalancedNodes -= it }
-    }
-
-    val unbalanceChildren = unbalancedNodes.first().children
-
-    val higherWeight = unbalanceChildren.maxOf { it.totalWeight }
-    val lowerWeight = unbalanceChildren.minOf { it.totalWeight }
-    val result2 = if (additionalWeight > 0) {
-        unbalanceChildren.first { it.totalWeight == higherWeight }.weight - additionalWeight
-    } else {
-        unbalanceChildren.first { it.totalWeight == lowerWeight }.weight - additionalWeight
-    }
-    println(result2)
-}
-
-private class Node(val name: String) {
-    var weight: Int = 0
-
-    var parent: Node? = null
-
-    val children = arrayListOf<Node>()
-
-    private var _totalWeight: Int? = null
-
-    fun clearTotalWeight() {
-        _totalWeight = null
-
-        children.forEach { it.clearTotalWeight() }
-    }
-
-    val totalWeight: Int
-        get() {
-            if (_totalWeight == null) {
-                _totalWeight = weight + children.sumOf { it.totalWeight }
+                else -> {
+                    children.forEach {
+                        dfs(it.key, null)?.also {
+                            return it
+                        }
+                    }
+                }
             }
-            return _totalWeight!!
+
+            return null
         }
+
+        part2Result = dfs(part1Result, null) ?: 0
+    }
 }
