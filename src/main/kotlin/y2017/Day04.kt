53b2b9bf0b0ad0a90178d0992146aba1c516e1fc
isaiahliu <isaiahliu@outlook.com>
2025-10-09 13:48:57 +0800

    1

--- src/main/kotlin/y2017/Day04.kt
+++ src/main/kotlin/y2017/Day04.kt
@@ -1,17 +1,45 @@
 package y2017
 
+import util.expect
 import util.input
 
 fun main() {
-    val part1Result = input.map { it.split(" ") }.count {
-        it.size == it.distinct().size
+    expect(0, 0) {
+        input.forEach {
+            val str1 = StringBuilder()
+            val str2 = IntArray(26)
+            val set1 = hashSetOf<String>()
+            val set2 = hashSetOf<String>()
+
+            var valid1 = 1
+            var valid2 = 1
+            "${it} ".forEach {
+                if (it == ' ') {
+                    if (!set1.add(str1.toString())) {
+                        valid1 = 0
+                    }
+
+                    if (!set2.add(buildString {
+                            str2.forEachIndexed { index, count ->
+                                repeat(count) {
+                                    append('a' + index)
+                                }
+
+                                str2[index] = 0
+                            }
+                        })) {
+                        valid2 = 0
+                    }
+
+                    str1.clear()
+                } else {
+                    str1.append(it)
+                    str2[it - 'a']++
+                }
+            }
+
+            part1Result += valid1
+            part2Result += valid2
+        }
     }
-
-    println(part1Result)
-
-    val part2Result = input.map { it.split(" ").map { String(it.toCharArray().sorted().toCharArray()) } }.count {
-        it.size == it.distinct().size
-    }
-
-    println(part2Result)
 }
