9f5ace6c105d984595748a2a7064a545d188977b
isaiahliu <isaiahliu@outlook.com>
2025-10-20 11:14:28 +0800

    1

--- src/main/kotlin/y2017/Day08.kt
+++ src/main/kotlin/y2017/Day08.kt
@@ -1,55 +1,55 @@
 package y2017
 
+import util.expect
 import util.input
 
 fun main() {
-    val registers = hashMapOf<String, Int>()
+    expect(0, Int.MIN_VALUE) {
+        val registers = hashMapOf<String, Int>()
 
-    fun String.evaluate(): Int {
-        return this.toIntOrNull() ?: registers.computeIfAbsent(this) { 0 }
-    }
+        fun String.evaluate(): Int {
+            return this.toIntOrNull() ?: registers.computeIfAbsent(this) { 0 }
+        }
 
-    var result2 = Int.MIN_VALUE
-    input.map { it.split(" ") }.forEach {
-        val (reg, incOp, incParam) = it
-        val (opParam1, op, opParam2) = it.takeLast(3)
+        input.map { it.split(" ") }.forEach {
+            val (reg, incOp, incParam) = it
+            val (opParam1, op, opParam2) = it.takeLast(3)
 
-        val predicate = when (op) {
-            ">" -> {
-                opParam1.evaluate() > opParam2.evaluate()
+            val predicate = when (op) {
+                ">" -> {
+                    opParam1.evaluate() > opParam2.evaluate()
+                }
+
+                "<" -> {
+                    opParam1.evaluate() < opParam2.evaluate()
+                }
+
+                ">=" -> {
+                    opParam1.evaluate() >= opParam2.evaluate()
+                }
+
+                "<=" -> {
+                    opParam1.evaluate() <= opParam2.evaluate()
+                }
+
+                "!=" -> {
+                    opParam1.evaluate() != opParam2.evaluate()
+                }
+
+                else -> {
+                    opParam1.evaluate() == opParam2.evaluate()
+                }
             }
 
-            "<" -> {
-                opParam1.evaluate() < opParam2.evaluate()
-            }
+            if (predicate) {
+                val inc = if (incOp == "inc") 1 else -1
 
-            ">=" -> {
-                opParam1.evaluate() >= opParam2.evaluate()
-            }
+                registers[reg] = (registers[reg] ?: 0) + inc * incParam.evaluate()
 
-            "<=" -> {
-                opParam1.evaluate() <= opParam2.evaluate()
-            }
-
-            "!=" -> {
-                opParam1.evaluate() != opParam2.evaluate()
-            }
-
-            else -> {
-                opParam1.evaluate() == opParam2.evaluate()
+                part2Result = maxOf(part2Result, registers[reg] ?: 0)
             }
         }
 
-        if (predicate) {
-            val inc = if (incOp == "inc") 1 else -1
-
-            registers[reg] = (registers[reg] ?: 0) + inc * incParam.evaluate()
-
-            result2 = result2.coerceAtLeast(registers[reg] ?: 0)
-        }
+        part1Result = registers.values.max()
     }
-
-    println(registers.values.max())
-
-    println(result2)
 }
